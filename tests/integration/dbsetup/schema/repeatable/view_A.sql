c450ea3c6a39c5bd1f5107dddd98d4a8d31a2d25
Jeff Zohrab <jzohrab@gmail.com>
2023-09-19 15:30:09 -0700

    Continue test.

---
+++ tests/integration/dbsetup/schema/repeatable/view_A.sql
@@ -0,0 +1,3 @@
+drop view if exists view_A;
+
+create view view_A as select * from A;
--- tests/integration/dbsetup/test_Setup.py
+++ tests/integration/dbsetup/test_Setup.py
@@ -2,9 +2,11 @@
 DB setup tests using fake baseline, migration files.
 """
 
+import os
+import pytest
 from lute.dbsetup.setup import Setup
 
-def test_happy_path_no_existing_database():
+def test_happy_path_no_existing_database(tmp_path):
     """
     If no db exists, setup should:
     - create the db using the baseline
@@ -12,4 +14,20 @@ def test_happy_path_no_existing_database():
     - no backup created
     """
 
-    assert 1 == 2
+    dbfile = tmp_path / 'testdb.db'
+    assert os.path.exists(dbfile) == False, 'no db exists'
+
+    d = tmp_path / 'backups'
+    d.mkdir()
+
+    thisdir = os.path.dirname(os.path.realpath(__file__))
+    baselinedir = os.path.join(thisdir, 'schema', 'baseline')
+    migdir = os.path.join(thisdir, 'schema', 'migrations')
+    repeatable = os.path.join(thisdir, 'schema', 'repeatable')
+
+
+    assert os.path.exists(dbfile), 'db was created'
+
+    # todo: check tables exist
+    # migrations run
+    # no backup
