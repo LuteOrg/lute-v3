0896c89fcf1be19e365d23eb361e220dab1c2d46
Jeff Zohrab <jzohrab@gmail.com>
2023-09-19 15:50:40 -0700

    Better init.

--- lute/dbsetup/setup.py
+++ lute/dbsetup/setup.py
@@ -6,15 +6,35 @@ Runs migrations.
 Manages backups pre-migration.a
 """
 
+import os
+
 class Setup: # pylint: disable=too-few-public-methods
     """
     Main setup class, coordinates other classes.
     """
 
-    def __init__(self, db_filename, backup_folder, migrations_folder, repeatable_migrations_folder):
-        pass
+    def __init__(
+            self,
+            db_filename: str,
+            backup_folder: str,
+            migration_files: dict
+    ):
+        self._db_filename = db_filename
+        self._backup_folder = backup_folder
+        self._migrations = migration_files
+
 
     def setup(self):
         """
         Do database setup, making backup if necessary, running migrations.
         """
+
+        if not os.path.exists(self._db_filename):
+            self._create_baseline()
+
+
+    def _create_baseline(self):
+        """
+        Create baseline database.
+        """
+        # get connection, run script
--- tests/integration/dbsetup/test_Setup.py
+++ tests/integration/dbsetup/test_Setup.py
@@ -17,14 +17,21 @@ def test_happy_path_no_existing_database(tmp_path):
     dbfile = tmp_path / 'testdb.db'
     assert os.path.exists(dbfile) == False, 'no db exists'
 
-    d = tmp_path / 'backups'
-    d.mkdir()
+    backups = tmp_path / 'backups'
+    backups.mkdir()
 
     thisdir = os.path.dirname(os.path.realpath(__file__))
-    baselinedir = os.path.join(thisdir, 'schema', 'baseline')
+    baseline = os.path.join(thisdir, 'schema', 'baseline', 'schema.sql')
     migdir = os.path.join(thisdir, 'schema', 'migrations')
     repeatable = os.path.join(thisdir, 'schema', 'repeatable')
+    migrations = {
+        'baseline': baseline,
+        'migrations': migdir,
+        'repeatable': repeatable
+    }
 
+    setup = Setup(dbfile, backups, migrations)
+    setup.setup()
 
     assert os.path.exists(dbfile), 'db was created'
 
