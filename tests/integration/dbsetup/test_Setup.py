ff514ded98d7bb75c281a71cbf817d3dc277d5fc
Jeff Zohrab <jzohrab@gmail.com>
2023-09-19 23:39:39 -0700

    Adding backup manager checks.

--- tests/integration/dbsetup/test_Setup.py
+++ tests/integration/dbsetup/test_Setup.py
@@ -3,13 +3,41 @@ DB setup tests using fake baseline, migration files.
 """
 
 import os
-import pytest
 import sqlite3
 from contextlib import closing
+import pytest
 
-from lute.dbsetup.setup import Setup, BackupManager
+from lute.dbsetup.setup import Setup
 
-def test_happy_path_no_existing_database(tmp_path):
+
+class MockBackupManager: # pylint: disable=too-few-public-methods
+    """
+    Fake backup manager to use for tests.
+    Can't figure out how to use monkeypatching or
+    mocking for objects, so this stub will do
+    for now.
+    """
+
+    def __init__(self):
+        self.backup_called = False
+
+    def handle_backup(self):
+        """
+        Record if backup was called.
+        """
+        self.backup_called = True
+
+
+@pytest.fixture(name="fakebackupmanager")
+def fixture_fakebackupmanager():
+    """
+    Fake object for tests.
+    """
+    return MockBackupManager()
+
+
+
+def test_happy_path_no_existing_database(tmp_path, fakebackupmanager):
     """
     If no db exists, setup should:
     - create the db using the baseline
@@ -18,7 +46,7 @@ def test_happy_path_no_existing_database(tmp_path):
     """
 
     dbfile = tmp_path / 'testdb.db'
-    assert os.path.exists(dbfile) == False, 'no db exists'
+    assert os.path.exists(dbfile) is False, 'no db exists'
 
     backups = tmp_path / 'backups'
     backups.mkdir()
@@ -34,9 +62,7 @@ def test_happy_path_no_existing_database(tmp_path):
         'repeatable': repeatable
     }
 
-    bm = BackupManager(dbfile, backups, 'nextfile.db.gz', 5)
-
-    setup = Setup(dbfile, bm, migrations)
+    setup = Setup(dbfile, fakebackupmanager, migrations)
     setup.setup()
 
     assert os.path.exists(dbfile), 'db was created'
@@ -50,10 +76,10 @@ def test_happy_path_no_existing_database(tmp_path):
         tnames = [t[0] for t in tnames]
         assert tnames == [ 'A', 'B' ], 'migrations run'
 
-    assert list(backups.glob('*.gz')) == [], 'no backups'
+    assert fakebackupmanager.backup_called is False, 'no backup called'
 
 
-def test_existing_database_new_migrations(tmp_path):
+def test_existing_database_new_migrations(tmp_path, fakebackupmanager):
     """
     If db exists, setup should:
     - run any migrations
@@ -62,7 +88,7 @@ def test_existing_database_new_migrations(tmp_path):
     """
 
     dbfile = tmp_path / 'testdb.db'
-    assert os.path.exists(dbfile) == False, 'no db exists'
+    assert os.path.exists(dbfile) is False, 'no db exists'
 
     thisdir = os.path.dirname(os.path.realpath(__file__))
     baseline = os.path.join(thisdir, 'schema', 'baseline', 'schema.sql')
@@ -70,7 +96,7 @@ def test_existing_database_new_migrations(tmp_path):
         sql = f.read()
     with closing(sqlite3.connect(dbfile)) as conn:
         conn.executescript(sql)
-    assert os.path.exists(dbfile) == True, 'db exists'
+    assert os.path.exists(dbfile) is True, 'db exists'
 
     backups = tmp_path / 'backups'
     backups.mkdir()
@@ -83,19 +109,12 @@ def test_existing_database_new_migrations(tmp_path):
         'repeatable': repeatable
     }
 
-    class FakeBackupManager:
-        def __init__(self):
-            self.backup_called = False
-        def handle_backup(self):
-            self.backup_called = True
-
-    fbm = FakeBackupManager()
-    setup = Setup(dbfile, fbm, migrations)
+    setup = Setup(dbfile, fakebackupmanager, migrations)
     setup.setup()
 
     assert os.path.exists(dbfile), 'db was created'
 
-    assert fbm.backup_called, 'backup was called'
+    assert fakebackupmanager.backup_called, 'backup was called'
 
     with closing(sqlite3.connect(dbfile)) as conn:
         cur = conn.cursor()
@@ -107,12 +126,15 @@ def test_existing_database_new_migrations(tmp_path):
         assert tnames == [ 'A', 'B' ], 'migrations run'
 
 
-def test_existing_database_no_new_migrations(tmp_path):
+def test_existing_database_no_new_migrations():
     """
     DB should be left as-is, no migrations run, no backup.
     """
+    a = 1
+    assert a == 2, 'to do'
 
-# TODO:
+
+# Tests to write:
 # BackupManager tests:
 # - backup created
 # - max number of files remain
