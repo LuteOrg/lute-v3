14d05df73deaec21139e31c505d08af89d37f0e0
Jeff Zohrab <jzohrab@gmail.com>
2023-10-03 08:56:39 -0700

    Initial draft form, post not working.

---
+++ lute/forms.py
@@ -0,0 +1,24 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SelectField
+from wtforms.validators import DataRequired, URL
+
+class LanguageForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    dict_1_uri = StringField('Dictionary 1', validators=[DataRequired()])
+    dict_2_uri = StringField('Dictionary 2')
+    sentence_translate_uri = StringField('Sentence translation', validators=[DataRequired()])
+    show_romanization = BooleanField('Show Romanization field')
+    right_to_left = BooleanField('Right-to-left')
+
+    parser_type = SelectField(
+        'Parse as',
+        choices=[
+            ('spacedel', 'Space Delimited'),
+            ('japanese', 'Japanese (MeCab)'),
+            ('classicalchinese', 'Classical Chinese'),
+        ])
+    character_substitutions = StringField('Character substitutions')
+
+    regexp_split_sentences = StringField('Split sentences at')
+    exceptions_split_sentences = StringField('Split sentence exceptions')
+    word_characters = StringField('Word characters')
--- lute/language.py
+++ lute/language.py
@@ -2,11 +2,13 @@
 /language endpoints.
 """
 
-from flask import Blueprint, render_template
-
+from flask import Blueprint, render_template, redirect, url_for, flash
 from lute.models.language import Language
+from lute.forms import LanguageForm
+from lute import db
 
 bp = Blueprint('language', __name__, url_prefix='/language')
+
 
 @bp.route('/index')
 def index():
@@ -15,3 +17,25 @@ def index():
     """
     languages = Language.query.all()
     return render_template('language/index.html', languages=languages)
+
+
+@bp.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    """
+    Edit a language.
+    """
+    language = Language.query.get(id)
+    if not language:
+        flash('Language not found', 'danger')
+        return redirect(url_for('language.index'))
+
+    form = LanguageForm(obj=language)
+
+    if form.validate_on_submit():
+        form.populate_obj(language)
+        db.session.add(language)
+        db.session.commit()
+        flash('Language updated successfully', 'success')
+        return redirect(url_for('language.index'))
+
+    return render_template('language/edit.html', form=form, language=language)
