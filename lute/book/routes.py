a5893c63d5bbac8a68407e3916c76a7f3983ad50
Jeff Zohrab <jzohrab@gmail.com>
2023-10-20 23:20:11 -0700

    Fix ui reference.

--- lute/book/routes.py
+++ lute/book/routes.py
@@ -5,13 +5,14 @@
 from flask import Blueprint, request, jsonify
 from lute.utils.data_tables import DataTablesFlaskParamParser
 from lute.models.book import Book
+from lute.book.datatables import get_data_tables_list
 
 bp = Blueprint('book', __name__, url_prefix='/book')
 
 def datatables_source(is_archived):
     "Get datatables json for books."
     parameters = DataTablesFlaskParamParser.parse_params(request.form)
-    data = Book.get_data_tables_list(parameters, is_archived)
+    data = get_data_tables_list(parameters, is_archived)
     return jsonify(data)
 
 
