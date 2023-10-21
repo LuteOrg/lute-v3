ae3e5a539ae4c0283de4d8a3f1caffdfd53f11cf
Jeff Zohrab <jzohrab@gmail.com>
2023-10-21 10:52:48 -0700

    Add initial field list.

--- lute/term/forms.py
+++ lute/term/forms.py
@@ -3,7 +3,7 @@ Flask-wtf forms.
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SelectField
+from wtforms import StringField, BooleanField, SelectField, HiddenField, FieldList
 from wtforms.validators import DataRequired
 
 
@@ -13,4 +13,23 @@ class TermForm(FlaskForm):
     """
 
     # TODO term form: add fields
+
+    # TODO term form: language - use predefined
+    language = SelectField(
+        'Language',
+        choices=[
+            (1, 'English'),
+        ])
+
+    # TODO term form: hide this, change to HiddenField
+    original_text = StringField('OriginalText')
     text = StringField('Text', validators=[DataRequired()], render_kw={"placeholder": "Term"})
+
+    parents = FieldList(StringField('parent'))
+
+    translation = StringField('Translation', render_kw={"placeholder": "Translation"})
+    romanization = StringField('Romanization', render_kw={"placeholder": "Pronunciation"})
+    status = StringField('Status', render_kw={"placeholder": "Status"})
+    termtags = FieldList(StringField('termtag'))
+    # TODO term form: hide this, change to HiddenField
+    current_image = StringField('OriginalText')
