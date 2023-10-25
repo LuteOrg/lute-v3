7ac0945cf9466bd1a75f5edbc1548da089fc215a
Jeff Zohrab <jzohrab@gmail.com>
2023-10-25 16:29:34 -0700

    Failing tests, demo management.

---
+++ lute/db/demo.py
@@ -0,0 +1,15 @@
+"""
+Functions to manage demo database.
+
+Lute db comes pre-loaded with some demo data.  User can view Tutorial,
+wipe data, etc.
+
+The db settings table contains a record, StKey = 'IsDemoData', if the
+data is demo.
+"""
+
+def contains_demo_data():
+    return True
+
+def remove_flag():
+    pass
