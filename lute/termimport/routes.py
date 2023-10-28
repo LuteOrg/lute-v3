dbf1d0b250d97705d36edbeda16d34ea04e4eda0
Jeff Zohrab <jzohrab@gmail.com>
2023-10-28 16:11:50 -0700

    Fix lint.

--- lute/termimport/routes.py
+++ lute/termimport/routes.py
@@ -4,11 +4,10 @@ Term import.
 
 import os
 import tempfile
-from flask import Blueprint, render_template, request, flash, redirect, url_for, Response
+from flask import Blueprint, render_template, flash, redirect
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField
 from wtforms.validators import DataRequired
-from werkzeug.utils import secure_filename
 from lute.termimport.service import import_file, BadImportFileError
 
 
@@ -17,11 +16,12 @@ bp = Blueprint('termimport', __name__, url_prefix='/termimport')
 
 class TermImportForm(FlaskForm):
     "Form for imports."
-    text_file = FileField('Text File')
+    text_file = FileField('Text File', validators=[DataRequired()])
 
 
 @bp.route('/index', methods=['GET', 'POST'])
 def term_import_index():
+    "Import posted file."
     form = TermImportForm()
 
     if form.validate_on_submit():
