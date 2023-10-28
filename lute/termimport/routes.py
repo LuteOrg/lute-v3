8334439fab39fd0d0600cccb647d807386896087
Jeff Zohrab <jzohrab@gmail.com>
2023-10-28 16:00:55 -0700

    Saving file to temp file.

--- lute/termimport/routes.py
+++ lute/termimport/routes.py
@@ -2,6 +2,8 @@
 Term import.
 """
 
+import os
+import tempfile
 from flask import Blueprint, render_template, request, flash, redirect, url_for, Response
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
@@ -25,13 +27,15 @@ def term_import_index():
     if form.validate_on_submit():
         text_file = form.text_file.data
         if text_file:
+            temp_file_name = tempfile.mkstemp()[1]
             try:
-                filename = secure_filename(text_file.filename)
-                text_file.save(filename)
-                stats = import_file(filename)
+                text_file.save(temp_file_name)
+                stats = import_file(temp_file_name)
                 flash(f"Imported {stats['created']} terms (skipped {stats['skipped']})", 'notice')
                 return redirect('/term/index', 302)
             except BadImportFileError as e:
                 flash(f"Error on import: {str(e)}", 'notice')
+            finally:
+                os.unlink(temp_file_name)
 
     return render_template('termimport/index.html', form=form)
