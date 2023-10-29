1035e2e085859242b560cb3d65739aa0833e48dd
Jeff Zohrab <jzohrab@gmail.com>
2023-10-29 10:10:45 -0700

    Setting model and tests.

--- lute/models/setting.py
+++ lute/models/setting.py
@@ -6,5 +6,43 @@ from lute.db import db
 
 class Setting(db.Model):
     "Settings table."
+    __tablename__ = 'settings'
     key = db.Column('StKey', db.String(40), primary_key=True)
     value = db.Column('StValue', db.String(40), nullable=False)
+
+
+    @staticmethod
+    def set_key(keyname, keyvalue):
+        "Set, but don't save, a setting."
+        s = db.session.query(Setting).filter(Setting.key == keyname).first()
+        if s is None:
+            s = Setting()
+            s.key = keyname
+        s.value = keyvalue
+        db.session.add(s)
+
+
+    @staticmethod
+    def get_key(keyname):
+        "Get the saved key, or None if it doesn't exist."
+        s = db.session.query(Setting).filter(Setting.key == keyname).first()
+        if s is None:
+            return None
+        return s.value
+
+
+    # Helpers for certain settings.
+
+    @classmethod
+    def get_last_backup_datetime(cls):
+        "Get the last_backup_datetime as int, or None."
+        v = Setting.get_key('lastbackup')
+        if v is None:
+            return None
+        return int(v)
+
+    @classmethod
+    def set_last_backup_datetime(cls, v):
+        "Set and save the last backup time."
+        Setting.set_key('lastbackup', v)
+        db.session.commit()
--- tests/unit/models/test_Setting.py
+++ tests/unit/models/test_Setting.py
@@ -7,28 +7,30 @@ from lute.models.setting import Setting
 from tests.dbasserts import assert_sql_result
 
 
-def test_save_and_retrieve():
+def test_save_and_retrieve(app_context):
+    "Smoke tests."
     sql = "select StValue from settings where StKey = 'zztrash'"
     assert_sql_result(sql, [], 'not set')
     Setting.set_key('zztrash', 42)
     assert_sql_result(sql, [], 'still not set')
-    db.session.commit()
 
+    db.session.commit()
     assert_sql_result(sql, [ '42' ], 'now set')
 
     v = Setting.get_key('zztrash')
     assert v == '42', 'is string'
 
 
-def test_missing_key_value_is_null():
+def test_missing_key_value_is_nullapp_context(app_context):
+    "Missing key = None."
     assert Setting.get_key('missing') is None, 'missing key'
 
 
-def test_smoke_last_backup():
-    v = Setting.last_backup_datetime()
+def test_smoke_last_backup(app_context):
+    "Check syntax only."
+    v = Setting.get_last_backup_datetime()
     assert v is None, 'not set'
 
-    settings_repo.last_backup_datetime = 42
-    v = Setting.last_backup_datetime()
+    Setting.set_last_backup_datetime(42)
+    v = Setting.get_last_backup_datetime()
     assert v == 42, 'set _and_ saved'
-
