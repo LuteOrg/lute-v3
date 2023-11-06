255a989b89ff8b01057b4ff237f35ecbaee2bc8f
Jeff Zohrab <jzohrab@gmail.com>
2023-11-05 20:43:28 -0800

    Simplify print statements in main.

--- lute/main.py
+++ lute/main.py
@@ -12,6 +12,7 @@ from lute.config.app_config import AppConfig
 logging.getLogger("waitress.queue").setLevel(logging.ERROR)
 logging.getLogger('natto').setLevel(logging.CRITICAL)
 
+
 def start():
     """
     Main entry point: Get the config, init the app, and start.
@@ -19,48 +20,44 @@ def start():
     If the config file doesn't exist, use the prod config.
     """
 
-    msg = """
-    Starting Lute:
-    """
+    def _print(s):
+        msg = "\n".join([
+            '  ' + lin.strip()
+            for lin in s.split("\n")
+        ])
+        print(msg, flush=True)
+
+    _print(['', 'Starting Lute:', ''])
 
     config_file = os.path.join(AppConfig.configdir(), 'config.yml')
     if not os.path.exists(config_file):
         prod_conf = os.path.join(AppConfig.configdir(), 'config.yml.prod')
         shutil.copy(prod_conf, config_file)
-        msg += """
-          Using new production config.
-        """
+        _print(['', 'Using new production config.', ''])
 
     app_config = AppConfig.create_from_config()
-    app = init_db_and_app(app_config)
+    app = init_db_and_app(app_config, output_func = _print)
 
     port = app_config.port
-    msg += f"""
-      data path: {app_config.datapath}
-      database: {app_config.dbfilename}
-    """
+    _print([f"data path: {app_config.datapath}",
+            f"database: {app_config.dbfilename}"])
 
     if app_config.is_docker:
-        msg += """
-    Lute is in a Docker container,
-    ensure data path is mounted to the host!
-        """
+        _print([
+            '',
+            'Lute is in a Docker container,',
+            'ensure data path is mounted to the host!'
+            ''])
 
-    msg += f"""
-    Running at:
+    _print(['',
+            'Running at:',
+            '',
+            f'http://localhost:{port}',
+            '',
+            "When you're finished reading, stop this process",
+            'with Ctrl-C or your system equivalent.',
+            ''])
 
-    http://localhost:{port}
-
-    When you're finished reading, stop this process
-    with Ctrl-C or your system equivalent.
-    """
-
-    msg = "\n".join([
-        '  ' + lin.strip()
-        for lin in msg.split("\n")
-    ])
-
-    print(msg)
     serve(app, host="0.0.0.0", port=port)
 
 
