82553bab71b3ed7e0449162e3098e7ca08e1d195
Jeff Zohrab <jzohrab@gmail.com>
2023-11-05 22:33:17 -0800

    Add version attribute.

---
+++ lute/__init__.py
@@ -0,0 +1,11 @@
+"""
+Version info.
+
+The version needs to be included in Lute itself, because Lute displays
+it in the application version screen.
+
+Flit pulls into the pyproject.toml using "dynamic".
+"""
+
+# TODO release: fix version number
+__version__ = "0.0.1.post4"
