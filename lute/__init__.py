26a84da4cb4150483760262003f8af309ae80020
Jeff Zohrab <jzohrab@gmail.com>
2023-11-10 00:12:12 -0800

    Version bump.

--- lute/__init__.py
+++ lute/__init__.py
@@ -1,11 +1,20 @@
 """
 Version info.
 
+Lute follows the version numbers at
+https://packaging.python.org/en/latest/specifications/version-specifiers/#version-specifiers
+
+e.g.
+
+3.0.0a1.dev1
+3.0.0a1
+3.0.0b1.dev1
+3.0.0
+
 The version needs to be included in Lute itself, because Lute displays
 it in the application version screen.
 
 Flit pulls into the pyproject.toml using "dynamic".
 """
 
-# TODO release: fix version number
-__version__ = "0.0.1.post8"
+__version__ = "3.0.0a1.dev1"
