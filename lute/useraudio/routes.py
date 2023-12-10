c09adb6c1c0e18d8d3ae20fd980e61c7b339f9cf
Jeff Zohrab <jzohrab@gmail.com>
2023-12-10 11:48:32 +0800

    No caching of audio file.

--- lute/useraudio/routes.py
+++ lute/useraudio/routes.py
@@ -13,8 +13,8 @@ bp = Blueprint("useraudio", __name__, url_prefix="/useraudio")
 
 @bp.route("/stream/<int:bookid>", methods=["GET"])
 def stream(bookid):
-    "Serve the image from the data/userimages directory."
+    "Serve the audio, no caching."
     dirname = current_app.env_config.useraudiopath
     b = Book.find(bookid)
     fname = os.path.join(dirname, b.audio_filename)
-    return send_file(fname, as_attachment=True)
+    return send_file(fname, as_attachment=True, max_age=0)
