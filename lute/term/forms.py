920c2260a949f032771a13024135c23a48c1a594
WebOfPies <70017511+webofpies@users.noreply.github.com>
2023-12-14 18:13:22 +0400

    status form design and move to btm. img hover xform

--- lute/term/forms.py
+++ lute/term/forms.py
@@ -35,7 +35,7 @@ class TermForm(FlaskForm):
         "Romanization", render_kw={"placeholder": "Pronunciation"}
     )
 
-    status_choices = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (99, "Wkn"), (98, "Ign")]
+    status_choices = [(1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (99, "✔"), (98, "🛇")]
     status = RadioField("Status", choices=status_choices)
 
     term_tags = FieldList(StringField("term_tags"))
