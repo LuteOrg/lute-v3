a1ce803367763da348efc4c9b8b10da31fdfb80f
WebOfPies <70017511+webofpies@users.noreply.github.com>
2023-12-16 04:03:39 +0400

    read grid start

--- lute/static/js/type.js
+++ lute/static/js/type.js
@@ -1,23 +1,40 @@
 "use strict";
 
+let textItems;
 const typeControlContainer = document.querySelector("#type-control-container");
 const typeButton = document.querySelector("#text-control-btn");
 const typePlusButton = document.querySelector(".type-plus");
 const typeMinusButton = document.querySelector(".type-minus");
 const fontField = document.querySelector(".type-control-size input");
-// const textItems = document.querySelectorAll("span.textitem");
 
 const lhPlusButton = document.querySelector(".lh-plus");
 const lhMinusButton = document.querySelector(".lh-minus");
 const lhField = document.querySelector(".type-control-lh input");
 
-// console.log(`items are ${textItems[0]}`);
+const widthPlusButton = document.querySelector(".width-plus");
+const widthMinusButton = document.querySelector(".width-minus");
 
-typeButton.addEventListener("click", ()=> {
-  const textItem = document.querySelector("span.textitem");
-  // console.log(getFontSize(textItem));
-  fontField.value = `${getFontSize(textItem)}px`;
-  lhField.value = `${getLineHeight(textItem)}`;
+const readGridContainer = document.querySelector("#read_grid_container");
+const theText = document.querySelector("#thetext");
+
+const domObserver = new MutationObserver((mutationList, observer) => {
+  textItems = document.querySelectorAll("span.textitem");
+
+  // if (textItems) console.log(textItems);
+
+  observer.disconnect();
+});
+
+domObserver.observe(theText, {childList: true, subtree: true});
+// console.log(`items are ${textItems[0]}`);
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
+typeButton.addEventListener("click", (e)=> {
+  // const textItem = document.querySelector("span.textitem");
+  // console.log(textItems);
+  e.stopPropagation();
+  fontField.value = `${getFontSize(textItems[0])}px`;
+  lhField.value = `${getLineHeight(textItems[0])}`;
 })
 
 
@@ -66,19 +83,30 @@ lhMinusButton.addEventListener("click", () => {
   resizeLineHeight("-");
 })
 
+widthPlusButton.addEventListener("click", () => {
+  changeTextWidth();
+})
+
 fontField.addEventListener("change", (e) => {
-  const textItems = document.querySelectorAll("span.textitem");
+  // const textItems = document.querySelectorAll("span.textitem");
   const size = `${parseFloat(e.target.value)}px`;
-  console.log(size);
+  // console.log(size);
 
   textItems.forEach((item) => {
     setFontSize(item, size);
   })
 })
 
+function getTextWidth() {
+  const elementComputedStyle = window.getComputedStyle(readGridContainer)
+  const [x, y] = elementComputedStyle.gridTemplateColumns.split(" ");
+  // console.log(parseFloat(x), parseFloat(y));
+  return [parseFloat(x), parseFloat(y)];
+}
+
 function getLineHeight(element) {
   const elementComputedStyle = window.getComputedStyle(element);
-  console.log((parseFloat(elementComputedStyle.marginBottom) * 2 + getFontSize(element)) / getFontSize(element));
+  // console.log((parseFloat(elementComputedStyle.marginBottom) * 2 + getFontSize(element)) / getFontSize(element));
   return Number(((parseFloat(elementComputedStyle.marginBottom) * 2 + getFontSize(element)) / getFontSize(element)).toPrecision(2));
   // return parseFloat(elementComputedStyle.marginBottom);
 }
@@ -100,14 +128,17 @@ function setFontSize(element, size) {
 }
 
 function resizeLineHeight(operation) {
-  
-  const textItems = document.querySelectorAll("span.textitem");
+  // const textItems = document.querySelectorAll("span.textitem");
   const currentSize = getLineHeight(textItems[0]);
 
   const add = (operation === "+");
-  const newSize = add ? currentSize + 0.1 : currentSize - 0.1;
+  let newSize = add ? currentSize + 0.1 : currentSize - 0.1;
+  // newSize = newSize < 1 ? 1 : newSize;
 
-  console.log(newSize, newSize.toPrecision(2));
+  // if (newSize < 1)
+  newSize = clamp(newSize, 1, 5);
+
+  // console.log(newSize, newSize.toPrecision(2));
   textItems.forEach((item) => {
     setLineHeight(item, Number(newSize.toPrecision(2)));
   })
@@ -115,7 +146,7 @@ function resizeLineHeight(operation) {
 
 function resizeFont(operation) {
   // console.log("click +");
-  const textItems = document.querySelectorAll("span.textitem");
+  // const textItems = document.querySelectorAll("span.textitem");
   const currentSize = getFontSize(textItems[0]);
   // console.log(`${parseFloat(currentSize) + 1} + px`);
   const add = (operation === "+");
@@ -123,4 +154,12 @@ function resizeFont(operation) {
   textItems.forEach((item) => {
     setFontSize(item, newSize);
   })
+}
+
+function changeTextWidth() {
+
+  const [x, y] = getTextWidth();
+  const newWidth = (x / (x + y)) + (x / (x + y)) * 0.05;
+
+  readGridContainer.style.gridTemplateColumns = `${newWidth}fr ${1 - newWidth}fr`;
 }
