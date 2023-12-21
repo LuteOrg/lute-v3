7e7fb3ec0fcd23c97c66e629c7ff09d264ed3388
Jeff Zohrab <jzohrab@gmail.com>
2023-12-21 14:01:50 +0700

    Move text options to slide-in menu.

--- lute/static/js/text-options.js
+++ lute/static/js/text-options.js
@@ -6,19 +6,14 @@ let lhDefault;
 let widthDefault;
 let columnDefault;
 
-const textOptionsContainer = document.querySelector("#text-options-container");
-const textButton = document.querySelector("#text-options-btn");
 const fontPlusButton = document.querySelector(".font-plus");
 const fontMinusButton = document.querySelector(".font-minus");
-const fontField = document.querySelector(".text-options-size input");
 
 const lhPlusButton = document.querySelector(".lh-plus");
 const lhMinusButton = document.querySelector(".lh-minus");
-const lhField = document.querySelector(".text-options-lh input");
 
 const widthPlusButton = document.querySelector(".width-plus");
 const widthMinusButton = document.querySelector(".width-minus");
-const widthField = document.querySelector(".text-options-width input");
 
 const oneColButton = document.querySelector(".column-one");
 const twoColButton = document.querySelector(".column-two");
@@ -41,10 +36,6 @@ const domObserver = new MutationObserver((mutationList, observer) => {
   const width = getFromLocalStorage("textWidth", widthDefault);
   const columnCount = getFromLocalStorage("columnCount", columnDefault);
 
-  fontField.value = `${fontSize}px`;
-  lhField.value = `${lhSize.toFixed(1)}`;
-  widthField.value = `${Math.round(width)}%`;
-
   textItems.forEach((item) => {
     setFontSize(item, `${convertPixelsToRem(fontSize)}rem`);
     setLineHeight(item, Number(lhSize.toPrecision(2)));
@@ -58,23 +49,6 @@ const domObserver = new MutationObserver((mutationList, observer) => {
 });
 
 domObserver.observe(theText, {childList: true, subtree: true});
-
-textButton.addEventListener("click", ()=> {
-  textOptionsContainer.classList.toggle("hide-text-options");
-});
-
-// stop propagation so clicking anything inside the popup
-// doesn't trigger text button click event
-textOptionsContainer.addEventListener("click", (e)=> {
-  e.stopPropagation();
-});
-
-// clicking away closes the popup
-document.addEventListener("click", (e) => {
-  if (!e.target.closest("#text-options-btn")) {
-    textOptionsContainer.classList.add("hide-text-options");
-  }
-});
 
 fontPlusButton.addEventListener("click", () => {
   resizeFont("+");
@@ -116,41 +90,6 @@ function changeColumnCount(num) {
   theText.style.columnCount = num;
   localStorage.setItem("columnCount", num);
 }
-
-fontField.addEventListener("change", (e) => {
-  let size = parseFloat(e.target.value);
-  size = clamp(size, 1, 50);
-  e.target.value = `${size}px`;
-
-  textItems.forEach((item) => {
-    setFontSize(item, `${convertPixelsToRem(size)}rem`);
-  });
-
-  localStorage.setItem("fontSize", size);
-});
-
-lhField.addEventListener("change", (e) => {
-  let size = parseFloat(e.target.value);
-  size = clamp(size, 1, 5);
-  e.target.value = size;
-
-  textItems.forEach((item) => {
-    setLineHeight(item, size);
-  });
-
-  localStorage.setItem("lineHeight", size.toPrecision(2));
-});
-
-widthField.addEventListener("change", (e) => {
-  let size = parseFloat(e.target.value);
-  size = clamp(size, 25, 75);
-  e.target.value = `${size}%`;
-
-  readPaneLeft.style.width = `${size}%`;
-  readPaneRight.style.width = `${(100 - size) * getReadPaneWidthRatio()}%`;
-
-  localStorage.setItem("textWidth", size);
-});
 
 function getFontSize(element) {
   const elementComputedStyle = window.getComputedStyle(element);
@@ -194,7 +133,6 @@ function resizeLineHeight(operation) {
     setLineHeight(item, Number(newSize.toPrecision(2)));
   });
 
-  lhField.value = Number(newSize.toPrecision(2)).toFixed(1);
   localStorage.setItem("lineHeight", newSize.toPrecision(2));
 }
 
@@ -210,7 +148,6 @@ function resizeFont(operation) {
     setFontSize(item, `${convertPixelsToRem(newSize)}rem`);
   });
 
-  fontField.value = `${newSize}px`;
   localStorage.setItem("fontSize", newSize);
 }
 
@@ -225,7 +162,6 @@ function changeTextWidth(operation) {
   readPaneLeft.style.width = `${newWidth}%`;
   readPaneRight.style.width = `${(100 - newWidth) * getReadPaneWidthRatio()}%`;
 
-  widthField.value = `${Math.round(newWidth)}%`;
   localStorage.setItem("textWidth", newWidth);
 }
 
