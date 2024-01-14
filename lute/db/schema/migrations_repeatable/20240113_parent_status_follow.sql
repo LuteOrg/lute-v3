a121e9c71eb05ada3262a2721d4e92517409786a
Jeff Zohrab <jzohrab@gmail.com>
2024-01-14 21:14:12 +0700

    Status tests.

--- lute/db/schema/migrations_repeatable/20240113_parent_status_follow.sql
+++ lute/db/schema/migrations_repeatable/20240113_parent_status_follow.sql
@@ -8,12 +8,46 @@ BEGIN
     UPDATE words
     SET WoStatus = new.WoStatus
     WHERE WoID in (
-      select WpWoID from wordparents
-      group by WpWoID having count(*) = 1
+      -- single parent children that are following this term.
+      select WpWoID
+      from wordparents
+      inner join words on WoID = WpWoID
+      where WoFollowParent = 1
+      and WpParentWoID = old.WoID
+      group by WpWoID
+      having count(*) = 1
+
+      UNION
+
+      -- The parent of this term,
+      -- if this term has a single parent and has "follow parent"
+      select WpParentWoID
+      from wordparents
+      inner join words on WoID = WpWoID
+      where WoFollowParent = 1
+      and WoID = old.WoID
+      group by WpWoID
+      having count(*) = 1
+    );
+END;
+
+
+DROP TRIGGER IF EXISTS trig_wordparents_update_WoStatusChanged_parent_follow;
+
+CREATE TRIGGER trig_wordparents_update_WoStatusChanged_parent_follow
+AFTER INSERT ON wordparents
+FOR EACH ROW
+BEGIN
+    UPDATE words
+    SET WoStatus = (
+      select WoStatus from words where WoID = new.WpWoID
     )
-    AND WoID in (
-      select WpWoID from wordparents
-      where WpParentWoID = new.WoID
-    )
-    AND WoFollowParent = 1;
+    WHERE WoID = new.WpParentWoID
+    AND 1 = (
+      SELECT COUNT(*)
+      FROM wordparents
+      INNER JOIN words ON WoID = WpWoID
+      WHERE WoFollowParent = 1
+      AND WoID = new.WpWoID
+    );
 END;
