90b6e7c9303d20902f4d626b5a664a3e44bb6289
Jeff Zohrab <jzohrab@gmail.com>
2024-01-15 07:07:05 +0700

    Split repeatable migrations.

---
+++ lute/db/schema/migrations_repeatable/trig_wordparents_after_insert_update_parent_WoStatus_if_following.sql
@@ -0,0 +1,18 @@
+DROP TRIGGER IF EXISTS trig_wordparents_after_insert_update_parent_WoStatus_if_following;
+
+CREATE TRIGGER trig_wordparents_after_insert_update_parent_WoStatus_if_following
+AFTER INSERT ON wordparents
+BEGIN
+    UPDATE words
+    SET WoStatus = (
+      select WoStatus from words where WoID = new.WpWoID
+    )
+    WHERE WoID = new.WpParentWoID
+    AND 1 = (
+      SELECT COUNT(*)
+      FROM wordparents
+      INNER JOIN words ON WoID = WpWoID
+      WHERE WoFollowParent = 1
+      AND WoID = new.WpWoID
+    );
+END;
--- lute/db/schema/migrations_repeatable/trig_words_after_update_WoStatus_if_following_parent.sql
+++ lute/db/schema/migrations_repeatable/trig_words_after_update_WoStatus_if_following_parent.sql
@@ -30,39 +30,3 @@ BEGIN
       having count(*) = 1
     );
 END;
-
-
-DROP TRIGGER IF EXISTS trig_wordparents_after_insert_update_parent_WoStatus_if_following;
-
-CREATE TRIGGER trig_wordparents_after_insert_update_parent_WoStatus_if_following
-AFTER INSERT ON wordparents
-BEGIN
-    UPDATE words
-    SET WoStatus = (
-      select WoStatus from words where WoID = new.WpWoID
-    )
-    WHERE WoID = new.WpParentWoID
-    AND 1 = (
-      SELECT COUNT(*)
-      FROM wordparents
-      INNER JOIN words ON WoID = WpWoID
-      WHERE WoFollowParent = 1
-      AND WoID = new.WpWoID
-    );
-END;
-
-
-DROP TRIGGER IF EXISTS trig_wordparents_after_delete_change_WoFollowParent;
-
-CREATE TRIGGER trig_wordparents_after_delete_change_WoFollowParent
-BEFORE DELETE ON wordparents
-FOR EACH ROW
-BEGIN
-    UPDATE words
-    SET WoFollowParent = 0
-    WHERE WoID IN
-    (
-      select WpWoID from wordparents
-      where WpParentWoID = old.WpParentWoID
-    );
-END;
