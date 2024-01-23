88fc55e55434a4c7473627ea599dfb2bec0130d8
Jeff Zohrab <jzohrab@gmail.com>
2024-01-23 08:14:11 +0700

    Printing summary of steps.

--- lute/utils/debug_helpers.py
+++ lute/utils/debug_helpers.py
@@ -8,11 +8,14 @@ class DebugTimer:
     Helper to log time.
     """
 
-    def __init__(self, name):
+    global_step_map = {}
+
+    def __init__(self, name, display=True):
         self.start = time.process_time()
         self.curr_start = self.start
         self.name = name
         self.step_map = {}
+        self.display = display
         print(f"{name} timer started")
 
     def step(self, s):
@@ -22,7 +25,18 @@ class DebugTimer:
         total_step_elapsed = self.step_map.get(s, 0)
         total_step_elapsed += step_elapsed
         self.step_map[s] = total_step_elapsed
+
+        full_step_map_string = f"{self.name} {s}"
+        global_step_elapsed = DebugTimer.global_step_map.get(full_step_map_string, 0)
+        global_step_elapsed += step_elapsed
+        DebugTimer.global_step_map[full_step_map_string] = global_step_elapsed
+
         total_elapsed = n - self.start
+        self.curr_start = n
+
+        if not self.display:
+            return
+
         msg = " ".join(
             [
                 f"{self.name} {s}:",
@@ -32,7 +46,6 @@ class DebugTimer:
             ]
         )
         print(msg, flush=True)
-        self.curr_start = n
 
     def summary(self):
         "Print final step summary."
@@ -40,3 +53,15 @@ class DebugTimer:
         for k, v in self.step_map.items():
             print(f"  {k}: {v:.6f}", flush=True)
         print(f"end {self.name} summary --------------")
+
+    @classmethod
+    def clear_total_summary(cls):
+        cls.global_step_map = {}
+
+    @classmethod
+    def total_summary(cls):
+        "Print final step summary."
+        print("global summary ------------------")
+        for k, v in cls.global_step_map.items():
+            print(f"  {k}: {v:.6f}", flush=True)
+        print("end global summary --------------")
