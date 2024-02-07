63853b536c1a8a7f1f0d2524897c94f08ed3a2de
Jeff Zohrab <jzohrab@gmail.com>
2024-02-07 15:45:28 +0700

    Treat sentences like terms.

--- lute/db/schema/migrations/20231225_175159_create_languagedict_table.sql
+++ lute/db/schema/migrations/20231225_175159_create_languagedict_table.sql
@@ -3,6 +3,7 @@
 CREATE TABLE languagedicts (
   "LdID" INTEGER NOT NULL,
   "LdLgID" INTEGER NOT NULL,
+  "LdUseFor" VARCHAR(20) NOT NULL,
   "LdType" VARCHAR(20) NOT NULL,
   "LdDictURI" VARCHAR(200) NOT NULL,
   "LdSortOrder" INTEGER NOT NULL,
@@ -13,33 +14,40 @@ CREATE TABLE languagedicts (
 
 -- Copy existing dictionary data to new table.
 
--- dict 1
+-- TERMS: dict 1
 
 -- embedded
-insert into languagedicts (LdLgID, LdType, LdDictURI, LdSortOrder)
-select lgid, "embeddedhtml", LgDict1URI, 1
+insert into languagedicts (LdLgID, LdUseFor, LdType, LdDictURI, LdSortOrder)
+select lgid, "terms", "embeddedhtml", LgDict1URI, 1
 from languages where LgDict1URI is not null and LgDict1URI NOT LIKE '*%';
 
 -- popup
 insert into languagedicts (LdLgID, LdType, LdDictURI, LdSortOrder)
-select lgid, "popuphtml", substr(LgDict1URI,2), 2
+select lgid, "terms", "popuphtml", substr(LgDict1URI,2), 2
 from languages where LgDict1URI is not null and LgDict1URI LIKE '*%';
 
 
--- dict 2
+-- TERMS: dict 2
 
 -- embedded
 insert into languagedicts (LdLgID, LdType, LdDictURI, LdSortOrder)
-select lgid, "embeddedhtml", LgDict2URI, 2
+select lgid, "terms", "embeddedhtml", LgDict2URI, 2
 from languages where LgDict2URI is not null and LgDict2URI NOT LIKE '*%';
 
 -- popup
 insert into languagedicts (LdLgID, LdType, LdDictURI, LdSortOrder)
-select lgid, "popuphtml", substr(LgDict2URI,2), 2
+select lgid, "terms", "popuphtml", substr(LgDict2URI,2), 2
 from languages where LgDict2URI is not null and LgDict2URI LIKE '*%';
 
 
--- TODO -- have to check if starts w/ * or not, like the above.
--- update languages
--- set LgGoogleTranslateURI = substr(LgGoogleTranslateURI, 2)
--- where LgGoogleTranslateURI is not null and LgGoogleTranslateURI LIKE '*%';
+-- SENTENCES
+
+-- embedded
+insert into languagedicts (LdLgID, LdUseFor, LdType, LdDictURI, LdSortOrder)
+select lgid, "sentences", "embeddedhtml", LgGoogleTranslateURI, 3
+from languages where LgGoogleTranslateURI is not null and LgGoogleTranslateURI NOT LIKE '*%';
+
+-- popup
+insert into languagedicts (LdLgID, LdType, LdDictURI, LdSortOrder)
+select lgid, "sentences", "popuphtml", substr(LgGoogleTranslateURI,2), 3
+from languages where LgGoogleTranslateURI is not null and LgGoogleTranslateURI LIKE '*%';
