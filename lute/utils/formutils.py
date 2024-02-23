d4ffe0dfee673dd7d89183ab7c92a21a4c6b3aea
Jeff Zohrab <jzohrab@gmail.com>
2024-02-23 19:59:53 +0700

    Extract helper method.

--- lute/utils/formutils.py
+++ lute/utils/formutils.py
@@ -3,6 +3,7 @@ Common form methods.
 """
 
 from lute.models.language import Language
+from lute.models.setting import UserSetting
 from lute.db import db
 
 
@@ -16,6 +17,25 @@ def language_choices(dummy_entry_placeholder="-"):
     langs = db.session.query(Language).order_by(Language.name).all()
     supported = [lang for lang in langs if lang.is_supported]
     lang_choices = [(s.id, s.name) for s in supported]
-    if len(lang_choices) > 1:
+    # Add a dummy placeholder even if there are no languages.
+    if len(lang_choices) != 1:
         lang_choices = [(0, dummy_entry_placeholder)] + lang_choices
     return lang_choices
+
+
+def valid_current_language_id():
+    """
+    Get the current language id from UserSetting, ensuring
+    it's still valid.  If not, change it.
+    """
+    current_language_id = UserSetting.get_value("current_language_id")
+    current_language_id = int(current_language_id)
+
+    valid_language_ids = [int(p[0]) for p in language_choices()]
+    if current_language_id in valid_language_ids:
+        return current_language_id
+
+    current_language_id = valid_language_ids[0]
+    UserSetting.set_value("current_language_id", current_language_id)
+    db.session.commit()
+    return current_language_id
