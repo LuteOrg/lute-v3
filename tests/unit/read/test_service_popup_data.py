a71a68bbd4a62355df78697be71afdffdbe99f84
Jeff Zohrab <jzohrab@gmail.com>
2024-03-14 15:56:29 +0700

    Add sanity check tests.

--- tests/unit/read/test_service_popup_data.py
+++ tests/unit/read/test_service_popup_data.py
@@ -7,20 +7,72 @@ from lute.read.service import get_popup_data
 from lute.db import db
 
 
-def test_smoke(spanish, app_context):
+def test_term_with_no_parents(spanish, app_context):
     "Keep the lights on test, smoke only."
-    terms = ["perro", "gato", "un gato"]
-    for term in terms:
-        t = Term(spanish, term)
-        db.session.add(t)
+    t = Term(spanish, "gato")
+    db.session.add(t)
     db.session.commit()
 
     d = get_popup_data(t.id)
-    print(d)
+    assert d["parentdata"] == [], "no parents"
+    assert d["parentterms"] == "", "no parents"
+
+
+def test_single_parent_data_not_added_if_same_translation(spanish, app_context):
+    "Extra data added for display."
+    t = Term(spanish, "gato")
+    t.translation = "cat"
+    p = Term(spanish, "perro")
+    p.translation = "cat"
+    t.parents.append(p)
+    db.session.add(t)
+    db.session.commit()
+
+    d = get_popup_data(t.id)
+    assert d["parentdata"] == [], "no extra parent data"
+    assert d["parentterms"] == "perro", "one parent"
+
+
+def test_parent_data_added_if_parent_translation_is_different(spanish, app_context):
+    "Extra data added for display."
+    t = Term(spanish, "gato")
+    t.translation = "cat"
+    p = Term(spanish, "perro")
+    p.translation = "kitty"
+    t.parents.append(p)
+    db.session.add(t)
+    db.session.commit()
+
+    d = get_popup_data(t.id)
+    expected_p_data = {
+        "term": "perro",
+        "roman": None,
+        "trans": "kitty",
+        "tags": [],
+    }
+    assert d["parentdata"] == [expected_p_data], "extra parent data added"
+    assert d["parentterms"] == "perro", "one parent"
+
+
+def test_parent_data_always_added_if_multiple_parents(spanish, app_context):
+    "Extra data added for display."
+    t = Term(spanish, "gato")
+    t.translation = "cat"
+    p = Term(spanish, "perro")
+    p.translation = "kitty"
+    p2 = Term(spanish, "hombre")
+    p2.translation = "kitteh"
+    t.parents.append(p)
+    t.parents.append(p2)
+    db.session.add(t)
+    db.session.commit()
+
+    d = get_popup_data(t.id)
+    expected_p1_data = {"term": "perro", "roman": None, "trans": "kitty", "tags": []}
+    expected_p2_data = {"term": "hombre", "roman": None, "trans": "kitteh", "tags": []}
+    expected = [expected_p1_data, expected_p2_data]
+    assert d["parentdata"] == expected, "extra parent data added"
+    assert d["parentterms"] == "perro, hombre", "parents"
 
 
 # TODO tests:
-# no parent
-# single parent same translation - no extra parent data
-# single parent with different translation - extra data
-# multi parents - extra data
