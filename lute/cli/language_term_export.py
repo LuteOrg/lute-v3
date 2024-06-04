03619e0a371ff10073c46b7fc4cd3b8a7988c3f4
Jeff Zohrab <jzohrab@gmail.com>
2024-06-03 19:48:21 -0700

    Use renderer for language export.

--- lute/cli/language_term_export.py
+++ lute/cli/language_term_export.py
@@ -21,6 +21,7 @@ from collections import Counter
 from lute.db import db
 from lute.models.book import Book
 from lute.term.model import Repository
+from lute.read.render.service import get_paragraphs
 
 
 def get_dist(book, collector, termrepo, language_id):  # pylint: disable=too-many-locals
@@ -159,24 +160,88 @@ def generate_file(language_name, outfile_name):
     """
     Generate the datafile for the language.
     """
+    # pylint: disable=too-many-locals
+
     books = db.session.query(Book).all()
     books = [b for b in books if b.language.name == language_name]
     if len(books) == 0:
         print(f"No books for given language {language_name}, quitting.")
         sys.exit(0)
 
-    langid = books[0].language.id
+    lang = books[0].language
+    langid = lang.id
 
     repo = Repository(db)
     terms = {}
+
+    def _add_term_if_needed(t):
+        if t.text_lc in terms:
+            return
+        tag_list = ", ".join([tg.text for tg in t.term_tags])
+        if tag_list == "":
+            tag_list = "-"
+
+        zws = "\u200B"
+        terms[t.text_lc] = {
+            "sourceterm": t,
+            "term": t.text.replace(zws, ""),
+            "count": 0,
+            "familycount": 0,
+            "books": [],
+            "definition": t.translation or "-",
+            "status": t.status,
+            "children": [],
+            "tags": tag_list,
+        }
+
     for b in books:
-        get_dist(b, terms, repo, langid)
+        print(f"Loading data for book {b.title} ...")
+        i = 0
+        for text in b.texts:
+            i += 1
+            if i % 10 == 0:
+                print(f"  page {i} of {b.page_count()}", end="\r")
+            paragraphs = get_paragraphs(text.text, lang)
+            displayed_terms = [
+                ti.term
+                for para in paragraphs
+                for sentence in para
+                for ti in sentence.textitems
+                if ti.is_word and ti.term is not None
+            ]
+            for t in displayed_terms:
+                _add_term_if_needed(t)
+                e = terms[t.text_lc]
+                e["count"] += 1
+                e["familycount"] += 1
+                if b.title not in e["books"]:
+                    e["books"].append(b.title)
 
-    load_term_data(langid, terms, repo)
-    load_parent_data(langid, terms, repo)
-    outdata = get_output_data(terms)
+                for parent in t.parents:
+                    _add_term_if_needed(parent)
+                    p = terms[parent.text_lc]
+                    p["familycount"] += 1
+                    if b.title not in p["books"]:
+                        p["books"].append(b.title)
+                    if t.text_lc not in p["children"]:
+                        p["children"].append(t.text_lc)
 
-    ptsorted = sorted(outdata, key=lambda c: c["familycount"], reverse=True)
+    for _, hsh in terms.items():
+        hsh["books"] = ", ".join(list(set(hsh["books"])))
+        # children to child (count)
+        children = []
+        for key in hsh["children"]:
+            t = terms[key]
+            children.append({"count": t["count"], "term": t["sourceterm"].text})
+        csorted = sorted(children, key=lambda c: c["count"], reverse=True)
+        children_string = "; ".join([f"{c['term']} ({c['count']})" for c in csorted])
+        if children_string == "":
+            children_string = "-"
+        hsh["children"] = children_string
+
+    outdata = terms.values()
+
+    ptsorted = sorted(outdata, key=lambda x: (-x["familycount"], x["term"]))
     keys = [
         "term",
         "count",
