868d6689acfd3c2cd96fce7c3980848fa37f65fc
Jeff Zohrab <jzohrab@gmail.com>
2024-08-14 21:02:21 -0700

    Issue 465: only change status 0 term statuses if explicitly set.

--- lute/termimport/service.py
+++ lute/termimport/service.py
@@ -195,11 +195,6 @@ def _update_term_skip_parents(t, repo, rec):
         tags = list(map(str.strip, rec["tags"].split(",")))
         t.term_tags = [t for t in tags if t != ""]
 
-    # If this is a status 0 ("unknown") term, and it's being updated,
-    # then it's no longer unknown!
-    if t.status == 0:
-        t.status = 1
-
     repo.add(t)
 
 
@@ -268,13 +263,7 @@ def _do_import(
                 _import_term_skip_parents(repo, hsh, lang, new_as_unknowns)
                 created_terms.append(ts)
 
-            elif create_terms and t is not None and t.status == 0:
-                # A status 0 "unknown" term can be created, but really
-                # it's an update of an existing term.
-                _update_term_skip_parents(t, repo, hsh)
-                created_terms.append(ts)
-
-            elif update_terms and t is not None and t.status != 0:
+            elif update_terms and t is not None:
                 # Can only update existing terms.
                 _update_term_skip_parents(t, repo, hsh)
                 updated_terms.append(ts)
