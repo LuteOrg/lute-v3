6504c013334fb85d3581faeee70b3b118f4b185e
Jeff Zohrab <jzohrab@gmail.com>
2024-10-28 15:54:00 -0700

    Issue 434: New hotkeys to move to prev/next unknown word.

--- lute/settings/routes.py
+++ lute/settings/routes.py
@@ -186,7 +186,13 @@ def _get_categorized_hotkeys():
     """
 
     categorized_settings = {
-        "Navigation": ["hotkey_StartHover", "hotkey_PrevWord", "hotkey_NextWord"],
+        "Navigation": [
+            "hotkey_StartHover",
+            "hotkey_PrevWord",
+            "hotkey_NextWord",
+            "hotkey_PrevUnknownWord",
+            "hotkey_NextUnknownWord",
+        ],
         "Update status": [
             "hotkey_Status1",
             "hotkey_Status2",
@@ -245,6 +251,8 @@ def edit_shortcuts():
         "hotkey_StartHover": "Deselect all words",
         "hotkey_PrevWord": "Move to previous word",
         "hotkey_NextWord": "Move to next word",
+        "hotkey_PrevUnknownWord": "Move to previous unknown word",
+        "hotkey_NextUnknownWord": "Move to next unknown word",
         "hotkey_StatusUp": "Bump the status up by 1",
         "hotkey_StatusDown": "Bump that status down by 1",
         "hotkey_Bookmark": "Bookmark the current page",
