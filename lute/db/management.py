af4e1f384089a60cc7016e68fb4a09bd1c37e4b0
Jeff Zohrab <jzohrab@gmail.com>
2024-11-03 09:58:01 -0800

    Break circular dependency.

--- lute/db/management.py
+++ lute/db/management.py
@@ -2,9 +2,11 @@
 Db management.
 """
 
+import os
 from sqlalchemy import text
 from flask import current_app
-from lute.settings.current import load
+from lute.models.setting import UserSetting
+from lute.models.repositories import UserSettingRepository
 
 
 def delete_all_data(session):
@@ -25,4 +27,117 @@ def delete_all_data(session):
     for s in statements:
         session.execute(text(s))
     session.commit()
-    load(session, current_app.env_config.default_user_backup_path)
+    add_default_user_settings(session, current_app.env_config.default_user_backup_path)
+
+
+def _revised_mecab_path(repo):
+    """
+    Change the mecab_path if it's not found, and a
+    replacement is found.
+
+    Lute Docker images are built to be multi-arch, and
+    interestingly (annoyingly), mecab libraries are installed into
+    different locations depending on the architecture, even with
+    the same Dockerfile and base image.
+
+    Returns: new mecab path if old one is missing _and_
+    new one found, otherwise just return the old one.
+    """
+
+    mp = repo.get_value("mecab_path")
+    if mp is not None and os.path.exists(mp):
+        return mp
+
+    # See develop docs for notes on how to find the libmecab path!
+    candidates = [
+        # linux/arm64
+        "/lib/aarch64-linux-gnu/libmecab.so.2",
+        # linux/amd64
+        "/lib/x86_64-linux-gnu/libmecab.so.2",
+        # github CI, ubuntu-latest
+        "/lib/x86_64-linux-gnu/libmecab.so.2",
+    ]
+    replacements = [p for p in candidates if os.path.exists(p)]
+    if len(replacements) > 0:
+        return replacements[0]
+    # Replacement not found, leave current value as-is.
+    return mp
+
+
+def add_default_user_settings(session, default_user_backup_path):
+    """
+    Load missing user settings with default values.
+    """
+    repo = UserSettingRepository(session)
+
+    # These keys are rendered into the global javascript namespace var
+    # LUTE_USER_SETTINGS, so if any of these keys change, check the usage
+    # of that variable as well.
+    keys_and_defaults = {
+        "backup_enabled": True,
+        "backup_auto": True,
+        "backup_warn": True,
+        "backup_dir": default_user_backup_path,
+        "backup_count": 5,
+        "lastbackup": None,
+        "mecab_path": None,
+        "japanese_reading": "hiragana",
+        "current_theme": "-",
+        "custom_styles": "/* Custom css to modify Lute's appearance. */",
+        "show_highlights": True,
+        "current_language_id": 0,
+        # Behaviour:
+        "open_popup_in_new_tab": False,
+        "stop_audio_on_term_form_open": True,
+        "stats_calc_sample_size": 5,
+        # Keyboard shortcuts.  These have default values assigned
+        # as they were the hotkeys defined in the initial Lute
+        # release.
+        "hotkey_StartHover": "escape",
+        "hotkey_PrevWord": "arrowleft",
+        "hotkey_NextWord": "arrowright",
+        "hotkey_StatusUp": "arrowup",
+        "hotkey_StatusDown": "arrowdown",
+        "hotkey_Bookmark": "b",
+        "hotkey_CopySentence": "c",
+        "hotkey_CopyPara": "shift+c",
+        "hotkey_TranslateSentence": "t",
+        "hotkey_TranslatePara": "shift+t",
+        "hotkey_NextTheme": "m",
+        "hotkey_ToggleHighlight": "h",
+        "hotkey_ToggleFocus": "f",
+        "hotkey_Status1": "1",
+        "hotkey_Status2": "2",
+        "hotkey_Status3": "3",
+        "hotkey_Status4": "4",
+        "hotkey_Status5": "5",
+        "hotkey_StatusIgnore": "i",
+        "hotkey_StatusWellKnown": "w",
+        # New hotkeys.  These must have empty values, because
+        # users may have already setup their hotkeys, and we can't
+        # assume that a given key combination is free:
+        "hotkey_CopyPage": "",
+        "hotkey_DeleteTerm": "",
+        "hotkey_EditPage": "",
+        "hotkey_TranslatePage": "",
+        "hotkey_PrevUnknownWord": "",
+        "hotkey_NextUnknownWord": "",
+        "hotkey_PrevSentence": "",
+        "hotkey_NextSentence": "",
+    }
+    for k, v in keys_and_defaults.items():
+        if not repo.key_exists(k):
+            s = UserSetting()
+            s.key = k
+            s.value = v
+            session.add(s)
+    session.commit()
+
+    # Revise the mecab path if necessary.
+    # Note this is done _after_ the defaults are loaded,
+    # because the user may have already loaded the defaults
+    # (e.g. on machine upgrade) and stored them in the db,
+    # so we may have to _update_ the existing setting.
+    revised_mecab_path = _revised_mecab_path(repo)
+    repo.set_value("mecab_path", revised_mecab_path)
+    session.commit()
--- tests/unit/db/test_management.py
+++ tests/unit/db/test_management.py
@@ -5,10 +5,11 @@ Testing management functions.
 """
 
 import pytest
+from sqlalchemy import text
 from lute.db import db
 from lute.models.setting import UserSetting
 from lute.models.repositories import UserSettingRepository
-from lute.db.management import delete_all_data
+from lute.db.management import delete_all_data, add_default_user_settings
 from tests.dbasserts import assert_record_count_equals
 
 
@@ -53,3 +54,37 @@ def test_can_get_backup_settings_when_db_is_wiped(app_context):
     bs = repo.get_backup_settings()
     assert bs.backup_enabled, "backup is back to being enabled"
     assert bs.backup_dir is not None, "default restored"
+
+
+@pytest.fixture(name="us_repo")
+def fixture_usersetting_repo(app_context):
+    "Repo"
+    r = UserSettingRepository(db.session)
+    return r
+
+
+def test_user_settings_loaded_with_defaults(us_repo):
+    "Called on load."
+    db.session.execute(text("delete from settings"))
+    db.session.commit()
+    assert us_repo.key_exists("backup_dir") is False, "key removed"
+    add_default_user_settings(db.session, "blah")
+    assert us_repo.key_exists("backup_dir") is True, "key created"
+
+    # Check defaults
+    b = us_repo.get_backup_settings()
+    assert b.backup_enabled is True
+    assert b.backup_dir is not None
+    assert b.backup_auto is True
+    assert b.backup_warn is True
+    assert b.backup_count == 5
+
+
+def test_user_settings_load_leaves_existing_values(us_repo):
+    "Called on load."
+    us_repo.set_value("backup_count", 17)
+    db.session.commit()
+    assert us_repo.get_value("backup_count") == "17"
+    add_default_user_settings(db.session, "blah")
+    b = us_repo.get_backup_settings()
+    assert b.backup_count == 17, "still 17"
