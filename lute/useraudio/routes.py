0f21c4bd939620954f16a6536c1ead5f9dc44d33
Justin <jaydom28@gmail.com>
2024-11-09 23:56:05 -0500

    fixed audioroute code

--- lute/useraudio/routes.py
+++ lute/useraudio/routes.py
@@ -6,7 +6,7 @@ User audio files are stored in the database in books table.
 
 import os
 from flask import Blueprint, send_file, current_app
-from lute.models.book import Book
+from lute.models.repositories import BookRepository
 
 bp = Blueprint("useraudio", __name__, url_prefix="/useraudio")
 
@@ -15,6 +15,7 @@ bp = Blueprint("useraudio", __name__, url_prefix="/useraudio")
 def stream(bookid):
     "Serve the audio, no caching."
     dirname = current_app.env_config.useraudiopath
-    b = Book.find(bookid)
-    fname = os.path.join(dirname, b.audio_filename)
+    br = BookRepository(db.session)
+    book = br.find(bookid)
+    fname = os.path.join(dirname, book.audio_filename)
     return send_file(fname, as_attachment=True, max_age=0)
