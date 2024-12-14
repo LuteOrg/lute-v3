b68e53bf4959048294a2c188428e2090e9ac3654
Jeff Zohrab <jzohrab@gmail.com>
2024-12-14 15:06:55 -0600

    Complete bulk parent tests.

--- lute/term/service.py
+++ lute/term/service.py
@@ -5,6 +5,15 @@
 from lute.term.model import Repository
 
 
+class TermServiceException(Exception):
+    """
+    Raised if something bad:
+
+    - missing parent
+    etc.
+    """
+
+
 class Service:
     "Service."
 
@@ -13,11 +22,23 @@ class Service:
 
     def bulk_set_parent(self, parenttext, termids):
         "Set parent for all terms, replace existing."
+        if len(termids) == 0:
+            return
+
         parent = None
         repo = Repository(self.session)
         terms = [repo.load(tid) for tid in termids]
-        lang_id = terms[0].language_id
+
+        lang_ids = list(set([term.language_id for term in terms]))
+        if len(lang_ids) > 1:
+            raise TermServiceException("Terms not all the same language")
+
+        lang_id = lang_ids[0]
         parent = repo.find(lang_id, parenttext)
+
+        if parent is None:
+            msg = f"Parent {parenttext} not found."
+            raise TermServiceException(msg)
 
         for term in terms:
             if term.parents != [parenttext]:
--- tests/unit/term/test_service.py
+++ tests/unit/term/test_service.py
@@ -6,10 +6,15 @@ import pytest
 from lute.models.term import Term as DBTerm
 from lute.models.repositories import TermRepository
 from lute.db import db
-from lute.term.service import Service
+from lute.term.service import Service, TermServiceException
 from tests.utils import add_terms
 
 # Bulk parent update
+
+
+def assert_parents(termid, parray, msg=""):
+    newt = TermRepository(db.session).find(termid)
+    assert [tp.text for tp in newt.parents] == parray, msg
 
 
 def test_bulk_parent_update_smoke_test(app_context, spanish):
@@ -17,11 +22,44 @@ def test_bulk_parent_update_smoke_test(app_context, spanish):
     [t, p] = add_terms(spanish, ["t", "p"])
     svc = Service(db.session)
     svc.bulk_set_parent("p", [t.id])
-    newt = TermRepository(db.session).find(t.id)
-    assert [tp.text for tp in newt.parents] == ["p"], "P added as parent"
+    assert_parents(t.id, ["p"], "P added as parent")
 
 
-# test_parent_must_exist
-# test_no_terms_ok
-# test_all_terms_must_be_same_language
-# with pytest.raises(FileNotFoundError, match="No such file"):
+def test_bulk_parent_update_no_terms_ok(app_context, spanish):
+    "Update parent of term."
+    [t, p] = add_terms(spanish, ["t", "p"])
+    svc = Service(db.session)
+    svc.bulk_set_parent("p", [])
+    # ok.
+
+
+def test_existing_parent_replaced(app_context, spanish):
+    "Update parent of term."
+    [t, u, p] = add_terms(spanish, ["t", "u", "p"])
+    u.add_parent(t)
+    db.session.add(u)
+    db.session.commit()
+    assert_parents(u.id, ["t"], "initial parents")
+    assert_parents(t.id, [], "initial parents")
+
+    svc = Service(db.session)
+    svc.bulk_set_parent("p", [t.id, u.id])
+    assert_parents(u.id, ["p"], "updated")
+    assert_parents(t.id, ["p"], "updated")
+
+
+def test_bulk_parent_update_parent_must_exist(app_context, spanish):
+    "Update parent of term."
+    [t, p] = add_terms(spanish, ["t", "p"])
+    svc = Service(db.session)
+    with pytest.raises(TermServiceException, match="Parent xxx not found"):
+        svc.bulk_set_parent("xxx", [t.id])
+
+
+def test_bulk_parent_update_all_terms_must_be_same_lang(app_context, spanish, english):
+    "Update parent of term."
+    [t, p] = add_terms(spanish, ["t", "p"])
+    [e] = add_terms(english, ["e"])
+    svc = Service(db.session)
+    with pytest.raises(TermServiceException, match="Terms not all the same language"):
+        svc.bulk_set_parent("p", [t.id, e.id])
