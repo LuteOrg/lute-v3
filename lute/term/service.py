cdd0a61fb89e98539e6eabcaa32781d1dafb12d2
Jeff Zohrab <jzohrab@gmail.com>
2024-12-17 19:49:28 -0600

    wip

--- lute/term/service.py
+++ lute/term/service.py
@@ -3,7 +3,7 @@
 """
 
 from dataclasses import dataclass, field
-from lute.models.repositories import TermRepository
+from lute.models.repositories import TermRepository, TermTagRepository
 from lute.term.model import Repository
 
 
@@ -51,6 +51,20 @@ class Service:
         # parent is found either by the ID, or if that returns None, by a text search.
         if bulk_update_data.parent_id is not None:
             parent = repo.find(bulk_update_data.parent_id)
+        if parent is None and bulk_update_data.parent_text is not None:
+            modelrepo = Repository(self.session)
+            pmodel = modelrepo.find_or_new(lang_ids[0], bulk_update_data.parent_text)
+            modelrepo.add(pmodel)
+            modelrepo.commit()
+            # Re-load it to get its id.  ... wasteful, not concerned at the moment.
+            pmodel = modelrepo.find(lang_ids[0], bulk_update_data.parent_text)
+            parent = repo.find(pmodel.id)
+
+        ttrepo = TermTagRepository(self.session)
+        add_tags = [ttrepo.find_or_create_by_text(a) for a in bulk_update_data.add_tags]
+        remove_tags = [
+            ttrepo.find_or_create_by_text(a) for a in bulk_update_data.remove_tags
+        ]
 
         for term in terms:
             if bulk_update_data.remove_parents:
@@ -58,6 +72,18 @@ class Service:
             if parent is not None:
                 term.remove_all_parents()
                 term.add_parent(parent)
+
+            if (
+                bulk_update_data.change_status is True
+                and bulk_update_data.status_value is not None
+            ):
+                term.status = bulk_update_data.status_value
+
+            for tag in add_tags:
+                term.add_term_tag(tag)
+            for tag in remove_tags:
+                term.remove_term_tag(tag)
+
             self.session.add(term)
             self.session.commit()
 
--- tests/unit/term/test_service.py
+++ tests/unit/term/test_service.py
@@ -8,6 +8,7 @@ from lute.models.term import TermTag
 from lute.db import db
 from lute.term.service import Service, TermServiceException, BulkTermUpdateData
 from tests.utils import add_terms
+from tests.dbasserts import assert_sql_result
 
 # Bulk parent update
 
@@ -56,12 +57,83 @@ def test_add_parent_by_id(app_context, spanish):
     assert_stringized(t.id, expected, "parent added")
 
 
-# add a parent by id
-# add parent by text
-# add parent by id ignores text
-# remove all parents
-# leave status
-# change status
+def test_remove_all_parents(app_context, spanish):
+    [t, p] = add_terms(spanish, ["t", "p"])
+    bud = BulkTermUpdateData(term_ids=[t.id], parent_id=p.id)
+    _apply_updates(bud)
+    expected = {"parents": ["p"], "status": 1, "tags": []}
+    assert_stringized(t.id, expected, "parent added")
+
+    bud = BulkTermUpdateData(term_ids=[t.id], remove_parents=True)
+    _apply_updates(bud)
+    expected = {"parents": [], "status": 1, "tags": []}
+    assert_stringized(t.id, expected, "parent removed")
+
+
+def test_add_parent_by_text_existing_parent(app_context, spanish):
+    [t, p] = add_terms(spanish, ["t", "p"])
+    bud = BulkTermUpdateData(term_ids=[t.id], parent_text="p")
+    _apply_updates(bud)
+    expected = {"parents": ["p"], "status": 1, "tags": []}
+    assert_stringized(t.id, expected, "parent added")
+
+
+def test_add_parent_by_text_new_parent(app_context, spanish):
+    [t, p] = add_terms(spanish, ["t", "p"])
+    bud = BulkTermUpdateData(term_ids=[t.id], parent_text="newparent")
+    _apply_updates(bud)
+    expected = {"parents": ["newparent"], "status": 1, "tags": []}
+    assert_stringized(t.id, expected, "parent added")
+
+
+def test_add_parent_text_ignored_if_id_present(app_context, spanish):
+    [t, p] = add_terms(spanish, ["t", "p"])
+    bud = BulkTermUpdateData(term_ids=[t.id], parent_id=p.id, parent_text="ignored")
+    _apply_updates(bud)
+    expected = {"parents": ["p"], "status": 1, "tags": []}
+    assert_stringized(t.id, expected, "parent added")
+
+
+def test_set_status(app_context, spanish):
+    [t] = add_terms(spanish, ["t"])
+    bud = BulkTermUpdateData(term_ids=[t.id], change_status=True, status_value=4)
+    _apply_updates(bud)
+    expected = {"parents": [], "status": 4, "tags": []}
+    assert_stringized(t.id, expected, "status")
+
+
+def test_set_status_skipped_if_change_status_false(app_context, spanish):
+    [t] = add_terms(spanish, ["t"])
+    bud = BulkTermUpdateData(term_ids=[t.id], change_status=False, status_value=4)
+    _apply_updates(bud)
+    expected = {"parents": [], "status": 1, "tags": []}
+    assert_stringized(t.id, expected, "status unchanged")
+
+
+def test_bulk_update_tag_add_and_remove_smoke_test(app_context, spanish):
+    "Update parent of term."
+    [t, t2] = add_terms(spanish, ["t", "p"])
+    t.add_term_tag(TermTag("hello"))
+    t.add_term_tag(TermTag("there"))
+    db.session.add(t)
+    db.session.commit()
+
+    expected = {"parents": [], "status": 1, "tags": ["hello", "there"]}
+    assert_stringized(t.id, expected, "initial tags")
+
+    bud = BulkTermUpdateData(
+        term_ids=[t.id], add_tags=["hello", "cat"], remove_tags=["there", "dog"]
+    )
+    _apply_updates(bud)
+
+    expected = {"parents": [], "status": 1, "tags": ["cat", "hello"]}
+    assert_stringized(t.id, expected, "tags added,removed")
+
+    tagssql = "select TgText from tags order by TgText"
+    expected_tags = ["cat", "hello", "there"]
+    assert_sql_result(tagssql, expected_tags, "tag created and added if needed")
+
+
 # ... tag tests
 # all terms have to be of the same language
 # no updates = nothing happens, ok.
