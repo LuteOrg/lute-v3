40582bf0fd0bb42d0f44f89a9a58c27d1e38151f
Jeff Zohrab <jzohrab@gmail.com>
2025-01-20 11:03:51 -0600

    wip

--- lute/term/datatables.py
+++ lute/term/datatables.py
@@ -10,7 +10,7 @@ def get_data_tables_list(parameters, session):
 
     base_sql = """SELECT
     w.WoID as WoID, LgName, L.LgID as LgID, w.WoText as WoText, parents.parentlist as ParentText,
-    replace(replace(w.WoTranslation, CHAR(10), "<br />"), CHAR(13), '') as WoTranslation,
+    WoTranslation,
     WoRomanization,
     replace(wi.WiSource, '.jpeg', '') as WiSource,
     ifnull(tags.taglist, '') as TagList,
