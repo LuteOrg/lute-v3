69cd37e1cf8298775fcc0a6b4a83ce651ad4fdb0
Jeff Zohrab <jzohrab@gmail.com>
2025-01-31 15:14:18 -0600

    Update term listing to use new data.

--- lute/userimage/routes.py
+++ lute/userimage/routes.py
@@ -12,10 +12,9 @@ from flask import Blueprint, send_from_directory, current_app
 bp = Blueprint("userimages", __name__, url_prefix="/userimages")
 
 
-@bp.route("/<int:lgid>/<term>", methods=["GET"])
-def get_image(lgid, term):
+@bp.route("/<int:lgid>/<path:f>", methods=["GET"])
+def get_image(lgid, f):
     "Serve the image from the data/userimages directory."
     datapath = current_app.config["DATAPATH"]
     directory = os.path.join(datapath, "userimages", str(lgid))
-    filename = term + ".jpeg"
-    return send_from_directory(directory, filename)
+    return send_from_directory(directory, f)
