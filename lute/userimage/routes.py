36d2c3ac441d7c0df8e8fc15bf378ef3a20f9104
Jeff Zohrab <jzohrab@gmail.com>
2025-01-31 15:56:50 -0600

    Fix term image set and upload to use new format.

--- lute/userimage/routes.py
+++ lute/userimage/routes.py
@@ -1,9 +1,5 @@
 """
 User images routes.
-
-User images are stored in the database as /userimages/langid/term, but
-with no jpeg extension.  Reason: the old symfony code couldn't manage
-urls with periods.
 """
 
 import os
@@ -17,4 +13,6 @@ def get_image(lgid, f):
     "Serve the image from the data/userimages directory."
     datapath = current_app.config["DATAPATH"]
     directory = os.path.join(datapath, "userimages", str(lgid))
+    if not os.path.exists(os.path.join(directory, f)):
+        return ""
     return send_from_directory(directory, f)
