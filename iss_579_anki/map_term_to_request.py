68166e92d758b7965ff5dcd997d5f5fe0a1bf511
Jeff Zohrab <jzohrab@gmail.com>
2025-01-31 10:37:58 -0600

    extract func

--- iss_579_anki/map_term_to_request.py
+++ iss_579_anki/map_term_to_request.py
@@ -6,9 +6,113 @@ import requests
 import pyparsing as pp
 from pyparsing import (
     quotedString,
-    QuotedString,
     Suppress,
 )
+
+
+def build_ankiconnect_post_json(
+    term, mapping_string, img_root_dir, deck_name, model_name
+):
+    "Build post json for term using the mappings."
+
+    # capture text inside {{ }}
+    pattern = r"{{\s*(.*?)\s*}}"
+    keys = set(re.findall(pattern, mapping_string))
+    # print(keys)
+
+    media_actions = []
+
+    # For each key, eval the value, build the dict
+    plain_replacements = {
+        "id": term.termid,
+        "term": term.text,
+        "language": term.language,
+        "parents": ", ".join(term.parents),
+        "tags": ", ".join(term.tags),
+        "translation": term.translation,
+    }
+
+    calc_required = [k for k in keys if k not in plain_replacements]
+    # print(calc_required)
+
+    def get_filtered_tags(tagvals):
+        "Get tags matching the spec."
+        # tagvals is a pyparsing ParseResults, convert to strings.
+        real_tagvals = list(tagvals)
+        ftags = [t for t in term.tags if t in real_tagvals]
+        # print(f"got filtered tags {ftags}")
+        return ", ".join(ftags)
+
+    def handle_image(_):
+        if term.image is None:
+            return ""
+
+        # print(f"handling image with args: {args}")
+        new_filename = f"LUTE_TERM_{term.termid}.jpg"
+        hsh = {"filename": new_filename, "path": img_root_dir + term.image}
+        media_actions.append(hsh)
+        return f'<img src="{new_filename}">'
+
+    quotedString.setParseAction(pp.removeQuotes)
+    tag_matcher = (
+        Suppress("tags")
+        + Suppress("[")
+        + pp.delimitedList(quotedString)
+        + Suppress("]")
+    )
+    image_matcher = Suppress("image")
+
+    matcher = tag_matcher.set_parse_action(
+        get_filtered_tags
+    ) | image_matcher.set_parse_action(handle_image)
+
+    calc_replacements = {}
+
+    for c in calc_required:
+        parse_result = matcher.parseString(c).asList()
+        calc_replacements[c] = parse_result[0]
+    # print(calc_replacements)
+
+    replacements = {**plain_replacements, **calc_replacements}
+
+    final = mapping_string
+    for k, v in replacements.items():
+        escaped = re.escape(k)
+        pattern = rf"{{{{\s*{escaped}\s*}}}}"
+        # print(f"Replacing {k} => {v} using pattern {pattern}")
+        final = re.sub(pattern, f"{v}", final)
+
+    postjson = {}
+    mappings = [s.strip() for s in final.split("\n") if s.strip() != ""]
+    for s in mappings:
+        # print(f"handling '{s}'")
+        field, val = s.split(":", 1)
+        postjson[field.strip()] = val.strip()
+
+    # print("=" * 25)
+    # print(final)
+    # print("=" * 25)
+    # print(postjson)
+    # print("=" * 25)
+    # print(media_actions)
+    # print("=" * 25)
+
+    all_actions = [{"action": "storeMediaFile", "params": p} for p in media_actions]
+    all_actions.append(
+        {
+            "action": "addNote",
+            "params": {
+                "note": {
+                    "deckName": deck_name,
+                    "modelName": model_name,
+                    "fields": postjson,
+                    "tags": ["lute"] + term.tags,
+                }
+            },
+        }
+    )
+
+    return {"action": "multi", "params": {"actions": all_actions}}
 
 
 class Term:
@@ -24,15 +128,15 @@ class Term:
         self.translation = None
 
 
-term = Term()
-term.termid = 42
-term.text = "aucher"
-term.language = "German"
-term.parents = ["hello", "there"]
-term.translation = 'also "here"'
-term.tags = ["der", "blah", "xxx", "yyy"]
+the = Term()
+the.termid = 43
+the.text = "aucher"
+the.language = "German"
+the.parents = ["hello", "there"]
+the.translation = 'also "here"'
+the.tags = ["der", "blah", "xxx", "yyy"]
 # TODO - need to fix this URL trash stored in the table, should just store the filename
-term.image = "/userimages/3/Geschenk.jpeg"
+the.image = "/userimages/3/Geschenk.jpeg"
 
 # images served from userimages in app_config
 IMAGE_ROOT_DIR = "/Users/jeff/Documents/Projects/lute/data"
@@ -47,125 +151,17 @@ Sentence: some text {{ parents }} more text {{ parents }}
 Definition: {{ translation }}
 """
 
-print(test_string)
-
-# Define regex pattern to capture text inside {{ }}
-pattern = r"{{\s*(.*?)\s*}}"
-
-keys = set(re.findall(pattern, test_string))
-print(keys)
-
-# For each key, eval the value, build the dict
-
-plain_replacements = {
-    "id": term.termid,
-    "term": term.text,
-    "language": term.language,
-    "parents": ", ".join(term.parents),
-    "tags": ", ".join(term.tags),
-    "translation": term.translation,
-}
-
-calc_required = [k for k in keys if k not in plain_replacements]
-print(calc_required)
-
-
-def get_filtered_tags(tagvals):
-    "Get tags matching the spec."
-    # tagvals is a pyparsing ParseResults, convert to strings.
-    real_tagvals = list(tagvals)
-    ftags = [t for t in term.tags if t in real_tagvals]
-    # print(f"got filtered tags {ftags}")
-    return ", ".join(ftags)
-
-
-media_actions = []
-
-
-def handle_image(args):
-    if term.image is None:
-        return ""
-
-    print(f"handling image with args: {args}")
-    new_filename = f"LUTE_TERM_{term.termid}.jpg"
-    hsh = {"filename": new_filename, "path": IMAGE_ROOT_DIR + term.image}
-    media_actions.append(hsh)
-    return f'<img src="{new_filename}">'
-
-
-quoteval = QuotedString(quoteChar='"')
-quotedString.setParseAction(pp.removeQuotes)
-list_of_values = pp.delimitedList(quotedString)
-tagvallist = Suppress("[") + list_of_values + Suppress("]")
-tag_matcher = Suppress("tags") + tagvallist
-
-image_matcher = Suppress("image")
-image_match_result = image_matcher.set_parse_action(handle_image)
-
-
-matcher = tag_matcher.set_parse_action(
-    get_filtered_tags
-) | image_matcher.set_parse_action(handle_image)
-
-calc_replacements = {}
-
-for c in calc_required:
-    ret = matcher.parseString(c).asList()
-    calc_replacements[c] = ret[0]
-
-print(calc_replacements)
-
-
-replacements = {**plain_replacements, **calc_replacements}
-
-final = test_string
-for k, v in replacements.items():
-    escaped = re.escape(k)
-    pattern = rf"{{{{\s*{escaped}\s*}}}}"
-    print(f"Replacing {k} => {v} using pattern {pattern}")
-    final = re.sub(pattern, f"{v}", final)
-
-postjson = {}
-mappings = [s.strip() for s in final.split("\n") if s.strip() != ""]
-for s in mappings:
-    print(f"handling '{s}'")
-    field, val = s.split(":", 1)
-    postjson[field.strip()] = val.strip()
-
+p = build_ankiconnect_post_json(
+    the, test_string, IMAGE_ROOT_DIR, "zzTestAnkiConnect", "Basic_vocab"
+)
 print("=" * 25)
-print(final)
-print("=" * 25)
-print(postjson)
-print("=" * 25)
-print(media_actions)
+print(json.dumps(p, indent=2))
 print("=" * 25)
 
-post_media_actions = [{"action": "storeMediaFile", "params": p} for p in media_actions]
+do_post = False
 
-post_addnote = {
-    "action": "addNote",
-    "params": {
-        "note": {
-            "deckName": "zzTestAnkiConnect",
-            "modelName": "Basic_vocab",
-            "fields": postjson,
-            "tags": term.tags,
-        }
-    },
-}
-
-
-all_actions = post_media_actions
-all_actions.append(post_addnote)
-
-full_post = {"action": "multi", "params": {"actions": all_actions}}
-
-print("=" * 25)
-print(json.dumps(full_post, indent=2))
-print("=" * 25)
-
-
-# ANKI_CONNECT_URL = "http://localhost:8765"
-# ret = requests.post(ANKI_CONNECT_URL, json=full_post, timeout=5)
-# rj = ret.json()
-# print(rj)
+if do_post:
+    ANKI_CONNECT_URL = "http://localhost:8765"
+    ret = requests.post(ANKI_CONNECT_URL, json=p, timeout=5)
+    rj = ret.json()
+    print(rj)
