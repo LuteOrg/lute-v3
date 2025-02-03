9c0d98bea2b7a8c9d25017388b8bc4e6b873bc9d
Jeff Zohrab <jzohrab@gmail.com>
2025-02-03 13:50:55 -0600

    use lib

--- iss_579_anki/post_demo.py
+++ iss_579_anki/post_demo.py
@@ -19,23 +19,13 @@ python -m iss_579_anki.post_demo
 
 import os
 import re
-from typing import Callable, Iterable
 from dataclasses import dataclass
 import json
 import requests
 import pyparsing as pp
 from pyparsing import (
-    infixNotation,
-    opAssoc,
-    Keyword,
-    Word,
-    # ParserElement,
-    nums,
-    one_of,
     quotedString,
-    QuotedString,
     Suppress,
-    Literal,
 )
 from pyparsing.exceptions import ParseException
 
@@ -45,6 +35,8 @@ from lute.models.repositories import TermRepository
 from lute.term.model import ReferencesRepository
 import lute.app_factory
 from lute.db import db
+
+from lute.ankiexport.selector import evaluate_selector
 
 
 IMAGE_ROOT_DIR = "/Users/jeff/Documents/Projects/lute-v3/data/userimages"
@@ -58,16 +50,17 @@ class AnkiExportConfigurationError(Exception):
 
 
 def anki_is_running():
+    "True if Anki version check returns data."
     try:
         p = {"action": "version", "version": 6}
-        ret = requests.post(ANKI_CONNECT_URL, json=p, timeout=5)
-        rj = ret.json()
+        requests.post(ANKI_CONNECT_URL, json=p, timeout=5)
         return True
     except requests.exceptions.ConnectionError:
         return False
 
 
 def validate_mapping(m):
+    "Check that the mapping is good."
     verify_anki_model_exists(m["note_type"])
     verify_anki_deck_exists(m["deck_name"])
     mapping_array = mapping_as_array(m["mapping"])
@@ -77,6 +70,7 @@ def validate_mapping(m):
 
 
 def _verify_anki_item_exists(p, name, category_name):
+    "Check collection contains the given name."
     ret = requests.post(ANKI_CONNECT_URL, json=p, timeout=5)
     rj = ret.json()
     if name not in rj["result"]:
@@ -142,126 +136,6 @@ def verify_anki_model_fields_exist(model_name, fieldnames):
         raise AnkiExportConfigurationError(
             f"Bad field names: {', '.join(bad_field_names)}"
         )
-
-
-def evaluate_selector(s, term):
-    "Parse the selector, return True or False for the given term."
-    # pylint: disable=too-many-locals
-
-    print(f"selector: {s}")
-
-    def has_any_matching_tags(tagvals):
-        term_tags = [t.text for t in term.term_tags]
-        return any(e in term_tags for e in tagvals)
-
-    def matches_lang(lang):
-        return term.language.name == lang[0]
-
-    def check_has_images():
-        "True if term or any parent has image."
-        pi = [p.get_current_image() is not None for p in term.parents]
-        return term.get_current_image() is not None or any(pi)
-
-    def check_has(args):
-        "Check has:x"
-        has_item = args[0]
-        if has_item == "image":
-            return check_has_images()
-        raise RuntimeError(f"Unhandled has check for {has_item}")
-
-    def check_parent_count(args):
-        "Check parents."
-        opMap = {
-            "<": lambda a, b: a < b,
-            "<=": lambda a, b: a <= b,
-            ">": lambda a, b: a > b,
-            ">=": lambda a, b: a >= b,
-            "!=": lambda a, b: a != b,
-            "=": lambda a, b: a == b,
-            "==": lambda a, b: a == b,
-        }
-        opstring, val = args
-        oplambda = opMap[opstring]
-        pcount = len(term.parents)
-        return oplambda(pcount, val)
-
-    ### class BoolNot:
-    ###     "Not unary operator."
-    ###     def __init__(self, t):
-    ###         self.arg = t[0][1]
-    ###     def __bool__(self) -> bool:
-    ###         v = bool(self.arg)
-    ###         return not v
-    ###     def __str__(self) -> str:
-    ###         return "~" + str(self.arg)
-    ###     __repr__ = __str__
-
-    class BoolBinOp:
-        "Binary operation."
-        repr_symbol: str = ""
-        eval_fn: Callable[[Iterable[bool]], bool] = lambda _: False
-
-        def __init__(self, t):
-            self.args = t[0][0::2]
-
-        def __str__(self) -> str:
-            sep = f" {self.repr_symbol} "
-            return f"({sep.join(map(str, self.args))})"
-
-        def __bool__(self) -> bool:
-            return self.eval_fn(bool(a) for a in self.args)
-
-    class BoolAnd(BoolBinOp):
-        repr_symbol = "&"
-        eval_fn = all
-
-    class BoolOr(BoolBinOp):
-        repr_symbol = "|"
-        eval_fn = any
-
-    quoteval = QuotedString(quoteChar='"')
-    quotedString.setParseAction(pp.removeQuotes)
-    list_of_values = pp.delimitedList(quotedString)
-
-    tagvallist = Suppress("[") + list_of_values + Suppress("]")
-    tagcrit = tagvallist | quoteval
-
-    tag_matcher = Suppress("tags") + Suppress(":") + tagcrit
-
-    lang_matcher = Suppress("language") + Suppress(":") + quoteval
-
-    has_options = Literal("image")
-    has_matcher = Suppress("has") + Suppress(":") + has_options
-
-    comparison_op = one_of("< <= > >= != = == <>")
-    integer = Word(nums).setParseAction(lambda x: int(x[0]))
-
-    parent_count_matcher = (
-        Suppress("parents")
-        + Suppress(".")
-        + Suppress("count")
-        + comparison_op
-        + integer
-    )
-
-    and_keyword = Keyword("and")
-    or_keyword = Keyword("or")
-
-    multi_check = infixNotation(
-        tag_matcher.set_parse_action(has_any_matching_tags)
-        | lang_matcher.set_parse_action(matches_lang)
-        | has_matcher.set_parse_action(check_has)
-        | parent_count_matcher.set_parse_action(check_parent_count),
-        [
-            (and_keyword, 2, opAssoc.LEFT, BoolAnd),
-            (or_keyword, 2, opAssoc.LEFT, BoolOr),
-        ],
-    )
-
-    result = multi_check.parseString(s)
-    print(f"{result}, {result[0]}")
-    # print(bool(result[0]))
-    return bool(result[0])
 
 
 # pylint: disable=too-many-arguments,too-many-positional-arguments
@@ -451,10 +325,11 @@ def get_selected_post_data(db_session, term_ids, all_mapping_data):
 
 
 def verify_valid_mapping_parsing(m):
+    "Check mapping with a dummy Term."
     t = Term(Language(), "")
     refsrepo = None
     try:
-        p = build_ankiconnect_post_json(
+        build_ankiconnect_post_json(
             t,
             refsrepo,
             m["mapping"],
@@ -464,9 +339,10 @@ def verify_valid_mapping_parsing(m):
         )
     except ParseException as ex:
         msg = f'Invalid mapping value "{ex.line}". '
-        raise AnkiExportConfigurationError(msg + str(ex))
+        raise AnkiExportConfigurationError(msg + str(ex)) from ex
 
 
+# pylint: disable=too-many-locals
 def run_test():
     "Sample mapping and terms."
     gender_card_mapping = """\
@@ -542,6 +418,7 @@ def run_test():
     print(json.dumps(jsons, indent=2))
     print("=" * 25)
 
+    # pylint: disable=unreachable
     print("\n\nNOT POSTING")
     return
     for p in jsons:
