82085b1575684b6eb4d6652d7cdc9e2f4c8c3790
Jeff Zohrab <jzohrab@gmail.com>
2025-02-03 13:33:42 -0600

    status matcher

--- tests/unit/ankiexport/test_selector.py
+++ tests/unit/ankiexport/test_selector.py
@@ -16,6 +16,7 @@ def _make_test_term(spanish):
 
     term.add_term_tag(TermTag("masc"))
     term.add_term_tag(TermTag("xxx"))
+    term.status = 3
 
     db.session.add(term)
     db.session.commit()
@@ -26,6 +27,17 @@ def _make_test_term(spanish):
     "selector,expected",
     [
         ('language:"Spanish"', True),
+        ('language:"xxx"', False),
+        ("parents.count=1", True),
+        ("parents.count==1", True),
+        ("parents.count>=0", True),
+        ("parents.count>1", False),
+        ('tags:"masc"', True),
+        ('tags:"fem"', False),
+        ('tags:["fem", "masc"]', True),
+        ("status<=3", True),
+        ("status==1", False),
+        ('parents.count=1 and tags["fem", "masc"] and status<=3', True),
     ],
 )
 def test_selector(selector, expected, empty_db, spanish):
