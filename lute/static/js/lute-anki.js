54f94e203dea94459c3653986d0fc7d47a4893c9
Jeff Zohrab <jzohrab@gmail.com>
2025-02-05 13:36:57 -0600

    getting data

--- lute/static/js/lute-anki.js
+++ lute/static/js/lute-anki.js
@@ -4,7 +4,7 @@ const LuteAnki = (function() {
 
   /* copied verbatim from
    * https://foosoft.net/projects/anki-connect/index.html#miscellaneous-actions */
-  function _invoke(action, version, params={}) {
+  function _invoke(postdict) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.addEventListener('error', () => reject('failed to issue request'));
@@ -30,7 +30,7 @@ const LuteAnki = (function() {
         });
 
         xhr.open('POST', 'http://127.0.0.1:8765');
-        xhr.send(JSON.stringify({action, version, params}));
+        xhr.send(JSON.stringify(postdict));
     });
   }
 
@@ -39,8 +39,49 @@ const LuteAnki = (function() {
      * Queries anki and gets data.
      */
     get_anki_specs: async function(anki_connect_url) {
-      const result = await _invoke('deckNames', 6);
-      console.log(`got list of decks: ${result}`);
+      let p = {
+        "action": "multi",
+        "version": 6,
+        "params": {
+          "actions": [
+            { "action": "deckNames" },
+            { "action": "modelNames" },
+          ]
+        }
+      }
+      result = await _invoke(p);
+      console.log(`got: ${JSON.stringify(result, null, 2)}`)
+
+      const deck_names = result[0];
+      const note_types = result[1];
+
+      const getfieldnames_actions = note_types.map(nt => ({
+        "action": "modelFieldNames",
+        "params": {
+          "modelName": nt
+        }
+      }));
+      console.log(JSON.stringify(getfieldnames_actions, null, 2));
+
+      p = {
+        "action": "multi",
+        "version": 6,
+        "params": { "actions": getfieldnames_actions }
+      };
+      result = await _invoke(p);
+      console.log(`got: ${JSON.stringify(result, null, 2)}`)
+      const note_type_fields = {};
+      for (let i = 0; i < note_types.length; i++) {
+        note_type_fields[note_types[i]] = result[i];
+      }
+      console.log(`got: ${JSON.stringify(note_type_fields, null, 2)}`)
+
+      const ret = {
+        deck_names: deck_names,
+        note_types: note_type_fields,
+      };
+      console.log(`got: ${JSON.stringify(ret, null, 2)}`)
+      return ret;
     }
   };
 
