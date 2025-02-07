929ea530e250a4c9d7bc4b4c1691368357fc22f5
Jeff Zohrab <jzohrab@gmail.com>
2025-02-07 12:25:10 -0600

    move func

--- lute/static/js/lute-anki.js
+++ lute/static/js/lute-anki.js
@@ -145,6 +145,79 @@ const LuteAnki = (function() {
   }
 
   /**
+   * Each term may have a series of exports to post, e.g.
+   * { "export_1": post_1_json, "export_2": post_2_json },
+   * and each post_N_json can be several actions, e.g.
+   *
+   *  {
+   *    "export_1": {
+   *      "action": "multi",
+   *      "params": {
+   *        "actions": [
+   *          { "action": "storeMediaFile", ... },
+   *          { "action": "addNote", ... }
+   *        ]
+   *      }
+   *    },
+   *    "export_2": {
+   *      "action": "multi",
+   *      "params": {
+   *        "actions": [
+   *          { "action": "storeMediaFile", ... },
+   *          { "action": "addNote", ... }
+   *        ]
+   *      }
+   *    },
+   *
+   * All posts are sent separately, but the results are
+   * await all, and so look like this:
+   * [
+   *   [ post_1_A_result, post_1_B_result ],
+   *   [ post_2_A_result, post_2_B_result ],
+   * ]
+   *
+   * e.g.
+   * [
+   *   // This is the result for "export_1"
+   *   ["LUTE_TERM_1.jpg", 1738896726062],
+   *   // This is the result for "export_2"
+   *   ["LUTE_TERM_2.jpg", { "result": null, "error": "... duplicate" }]
+   * ]
+   *
+   * This maps the export names to the addNote actions, e.g. for the above, returns
+   * [
+   *   "export_1: success",          // No error in export_1's results
+   *   "export_2: "... duplicate"    // error in export_2's results
+   * ]
+   */
+  function _get_addNote_results(name_to_posts, results) {
+    // Converts each export name and inner action to an array, e.g.
+    // [{ name: "export_1", action: "storeMediaFile" } ... etc
+    const name_and_action_array = Object.entries(name_to_posts).flatMap(
+      ([name, obj]) =>
+      obj.params.actions.map(action => ({ name, action: action.action }))
+    );
+
+    // Maps all results to the error message, or success.
+    // For the exmaple in the function docs, this would return
+    // [ "success", "... duplicate" ]
+    const flat_results = results.flat().map(entry => 
+      entry && typeof entry === "object" && "error" in entry ?
+        entry.error : "success"
+    );
+
+    // Adds the final flat_result to its corresponding name_and_action_array.
+    for (let i = 0; i < name_and_action_array.length; i++) {
+      name_and_action_array[i]["result"] = flat_results[i];
+    }
+
+    return name_and_action_array
+      .filter(entry => entry.action === "addNote")
+      .map(entry => `${entry.name}: ${entry.result}`);
+  }
+
+
+  /**
    * Posts anki cards for the given word_ids, and calls the callback
    * for each.
    *
@@ -165,78 +238,6 @@ const LuteAnki = (function() {
         data: JSON.stringify(data),
         dataType: "json"
       });
-    }
-
-    /**
-     * Each term may have a series of exports to post, e.g.
-     * { "export_1": post_1_json, "export_2": post_2_json },
-     * and each post_N_json can be several actions, e.g.
-     *
-     *  {
-     *    "export_1": {
-     *      "action": "multi",
-     *      "params": {
-     *        "actions": [
-     *          { "action": "storeMediaFile", ... },
-     *          { "action": "addNote", ... }
-     *        ]
-     *      }
-     *    },
-     *    "export_2": {
-     *      "action": "multi",
-     *      "params": {
-     *        "actions": [
-     *          { "action": "storeMediaFile", ... },
-     *          { "action": "addNote", ... }
-     *        ]
-     *      }
-     *    },
-     *
-     * All posts are sent separately, but the results are
-     * await all, and so look like this:
-     * [
-     *   [ post_1_A_result, post_1_B_result ],
-     *   [ post_2_A_result, post_2_B_result ],
-     * ]
-     *
-     * e.g.
-     * [
-     *   // This is the result for "export_1"
-     *   ["LUTE_TERM_1.jpg", 1738896726062],
-     *   // This is the result for "export_2"
-     *   ["LUTE_TERM_2.jpg", { "result": null, "error": "... duplicate" }]
-     * ]
-     *
-     * This maps the export names to the addNote actions, e.g. for the above, returns
-     * [
-     *   "export_1: success",          // No error in export_1's results
-     *   "export_2: "... duplicate"    // error in export_2's results
-     * ]
-     */
-    function _get_addNote_results(name_to_posts, results) {
-      // Converts each export name and inner action to an array, e.g.
-      // [{ name: "export_1", action: "storeMediaFile" } ... etc
-      const name_and_action_array = Object.entries(name_to_posts).flatMap(
-        ([name, obj]) =>
-        obj.params.actions.map(action => ({ name, action: action.action }))
-      );
-
-      // Maps all results to the error message, or success.
-      // For the exmaple, this would return
-      // [ "success", "... duplicate" ]
-      const flat_results = results.flat().map(entry => 
-        entry && typeof entry === "object" && "error" in entry ?
-          entry.error : "success"
-      );
-
-      // Adds the final flat_result to its corresponding name_and_action_array.
-      for (let i = 0; i < name_and_action_array.length; i++) {
-        name_and_action_array[i]["result"] = flat_results[i];
-      }
-
-      return name_and_action_array
-        .filter(entry => entry.action === "addNote")
-        .map(entry => `${entry.name}: ${entry.result}`);
     }
 
     for (const [term_id, name_to_posts] of Object.entries(post_data)) {
