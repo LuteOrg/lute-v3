2de115a7156120f0fb2d266ed645b32ed0459564
Eternal Dreamer <mike.cod013@gmail.com>
2026-05-11 01:04:46 +0200

    style: titles style, truncate until first h1

--- lute/book/service.py
+++ lute/book/service.py
@@ -16,7 +16,7 @@ from openepub import Epub, EpubError
 from pypdf import PdfReader
 from subtitle_parser import SrtParser, WebVttParser
 from lute.book.model import Repository
-# import re
+import re
 
 
 class BookImportException(Exception):
@@ -214,18 +214,23 @@ class Service:
 
         soup = BeautifulSoup(s, "html.parser")
         extracted_text = []
-
+        truncated_to_h1 = False
+        
         # Add elements in order found.
         for element in soup.descendants:
+            element_text = element.text.strip()
             if element.text not in ("", None):
-                element_text = element.text # remove refs: re.sub(r"\[\d+\]", "", element.text)
+                # element_text = # remove refs: re.sub(r"\[\d+\]", "", element.text)
                 if element.name == "p":
                     extracted_text.append(element_text)
                 elif element.name in ("h1", "h2", "h3", "h4"):
-                    extracted_text.append(''.rjust(int(element.name[1]), '#') + ' ' + element_text)
+                    # Truncate until first h1 title
+                    if element.name == "h1" and not truncated_to_h1:
+                        truncated_to_h1 = True
+                        extracted_text = []
+                    else:
+                        extracted_text.append(''.rjust(int(element.name[1]), '#') + ' ' + element_text)
         
-        print(extracted_text)
-
         title_node = soup.find("title")
         orig_title = title_node.string if title_node else url
 
