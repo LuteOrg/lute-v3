e38f155871132478063c61859296fc3c5751bced
Jeff Zohrab <jzohrab@gmail.com>
2026-07-23 07:49:23 -0700

    Remove tracking array, only using single popup.

--- lute/static/js/lute-popups.js
+++ lute/static/js/lute-popups.js
@@ -5,32 +5,20 @@ const LutePopups = (function() {
   /* Open a new popup, or reuse existing. */
   function open_popup(url, settings) {
     const topWin = window.top;
-
     if (topWin.lute_popup_window && !topWin.lute_popup_window.closed) {
       topWin.lute_popup_window.location = url;
       topWin.lute_popup_window.focus();
     }
     else {
-      const pop = topWin.open(url, 'dictwin', settings);
-      topWin.lute_popup_window = pop;
-      if (!topWin.lute_popup_windows) {
-        topWin.lute_popup_windows = [];
-      }
-      topWin.lute_popup_windows.push(pop);
+      topWin.lute_popup_window = topWin.open(url, 'dictwin', settings);
     }
   }
 
   /* Close all popups. */
   function close_all_popups() {
-    if (window.top.lute_popup_windows) {
-      window.top.lute_popup_windows.forEach(function(w) {
-        if (w && !w.closed) {
-          w.close();
-        }
-      });
-    }
-    if (window.top.lute_popup_window && !window.top.lute_popup_window.closed) {
-      window.top.lute_popup_window.close();
+    const topWin = window.top;
+    if (topWin.lute_popup_window && !topWin.lute_popup_window.closed) {
+      topWin.lute_popup_window.close();
     }
   }
 
